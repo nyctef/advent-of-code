d19aa2cf3d58cadb261ea8239c3a183d97f35ebd
Nyctef <nyctef@nyctef.com>
2022-12-04 12:55:39 +0000

    day 2 part 2

--- day_2.py
+++ day_2.py
@@ -9,27 +9,49 @@ def individual_score(strat):
     return {"R": 1, "P": 2, "S": 3}[strat]
 
 
+beats = {"R": "S", "P": "R", "S": "P"}
+loses = {"R": "P", "P": "S", "S": "R"}
+
+
 def battle_score(opp, me):
     if opp == me:
         return 3
-    beats = {"R": "S", "P": "R", "S": "P"}
     if beats[me] == opp:
         return 6
     return 0
 
 
+def my_strat(opp, me):
+    if me == "X":
+        return beats[opp]
+    if me == "Y":
+        return opp
+    if me == "Z":
+        return loses[opp]
+
+
 with open("input/2-1.txt") as f:
     input_file = f.read()
+
+#
+# input_file = """A Y
+# B X
+# C Z
+# """
 
 lines = input_file.splitlines()
 strats = []
 for line in lines:
     [opp, me] = line.split()
-    strats.append((convert(opp), convert(me)))
-pprint(strats)
+    strats.append((convert(opp), me))
+# pprint(strats)
 
 total_score = 0
 for (opp, me) in strats:
+    pprint((opp, me))
+    me = my_strat(opp, me)
+    pprint(me)
     total_score += individual_score(me) + battle_score(opp, me)
+    pprint(total_score)
 
 print(total_score)
