f44055359047bb485cd0dae27945234d147511e2
Nyctef <nyctef@nyctef.com>
2022-12-10 06:49:50 +0000

    day 10 part 2

--- day_10.py
+++ day_10.py
@@ -18,29 +18,56 @@ def parse(line: str):
 
 instrs = input_file.splitlines()
 instrs = [parse(x) for x in instrs]
+# instrs = instrs[:15]
 
 clock = 0
 x = 1
-sum_strength = 0
+
+
+def asdf(*args, **kwargs):
+    pass
+
+
+draw = asdf
+log = print
+
+result = ""
 
 
 def cycle():
     global clock
+    global x
+    global result
     clock += 1
-    if clock in [20, 60, 100, 140, 180, 220]:
-        strength = clock * x
-        print(f"during cycle {clock=} {x=} {strength=}")
-        global sum_strength
-        sum_strength += strength
+    log()
+    log(f"during cycle {clock=} {x=}")
+    if abs((clock % 40) - 1 - x) < 2:
+        result += "#"
+        log(f"CRT draws a #")
+    else:
+        result += "."
+        log(f"CRT draws a .")
+        pass
+
+    if clock % 40 == 0:
+        result += "\n"
+        pass
 
 
 for (op, value) in instrs:
     if op == "noop":
+        log(f"start of instr: noop")
         cycle()
+        log(f"end of instr: noop")
     if op == "addx":
+        log(f"start of instr: addx {value}")
         cycle()
         cycle()
         x += value
+        log(f"end of instr: addx {value} (x is now {x})")
+
+# print()
+# print(sum_strength)
 
 print()
-print(sum_strength)
+print(result)
