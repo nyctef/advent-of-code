609547516a69287ccacb3bb68ddd3cd39870e575
Nyctef <nyctef@nyctef.com>
2022-12-13 08:04:05 +0000

    day 1 part 1

--- day_13.py
+++ day_13.py
@@ -1,4 +1,5 @@
 from itertools import zip_longest
+from pathlib import Path
 from pprint import pprint
 from typing import List
 
@@ -26,7 +27,12 @@ input_file = """[1,1,3,1,1]
 
 [1,[2,[3,[4,[5,6,7]]]],8,9]
 [1,[2,[3,[4,[5,6,0]]]],8,9]
+
+[[1, 2], 3]
+[[1, 2], 0]
 """
+
+input_file = Path("input/13-1.txt").read_text()
 
 inputs = input_file.split("\n\n")
 inputs = [tuple(map(eval, x.splitlines())) for x in inputs]
@@ -45,35 +51,55 @@ def compare(left: Foo, right: Foo):
     for (l, r) in zip_longest(left, right, fillvalue=None):
         if l is None:
             log("left side ran out of items, so correct")
-            return True
+            return "CORRECT"
         elif r is None:
             log("right side ran out of items, so not in right order")
-            return False
+            return "WRONG"
         elif isinstance(l, int) and isinstance(r, int):
             if r < l:
                 log(f"{r=} is less than {l=}, so wrong order")
-                return False
+                return "WRONG"
+            elif l < r:
+                log(f"{l=} < {r=}, so right order")
+                return "CORRECT"
             else:
-                log(f"{l=} <= {r=}, so right order")
+                log(f"{l=} = {r=}, so contining")
         elif isinstance(l, list) and isinstance(r, list):
             log(f"recursing into comparing {l=} and {r=}")
-            if not compare(l, r):
-                return False
+            r = compare(l, r)
+            if r is not "EQUAL":
+                return r
         elif isinstance(l, int) and isinstance(r, list):
             log(f"recursing into comparing {[l]=} and {r=}")
-            return compare([l], r)
+            r = compare([l], r)
+            if r is not "EQUAL":
+                return r
         elif isinstance(l, list) and isinstance(r, int):
             log(f"recursing into comparing {l=} and {[r]=}")
-            return compare(l, [r])
+            r = compare(l, [r])
+            if r is not "EQUAL":
+                return r
         else:
             raise Exception((l, r))
-    return True
+    return "EQUAL"
 
 
 pprint(inputs)
 
-for (i, (l, r)) in enumerate(inputs, start=1):
-    print((i, compare(l, r)))
+result = 0
 
-# print((8, inputs[7], compare(inputs[7][0], inputs[7][1])))
+for (i, (l, r)) in enumerate(inputs, start=1):
+    r = compare(l, r)
+    print((i, r))
+    if r != "WRONG":
+        result += i
+
+
+def debug(i):
+    print(i, inputs[i - 1], compare(inputs[i - 1][0], inputs[i - 1][1]))
+
+
+# debug(2)
 # print(list(zip_longest(inputs[4][0], inputs[4][1], fillvalue=None)))
+
+print(result)
