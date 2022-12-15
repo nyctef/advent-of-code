c6377c692a81398e1442557b708143d25ef11fcd
Nyctef <nyctef@nyctef.com>
2022-12-15 06:39:09 +0000

    day 15 wip

--- day_15.py
+++ day_15.py
@@ -1,6 +1,8 @@
+from collections import namedtuple
 from pathlib import Path
 from pprint import pprint
 import re
+from typing import List, Tuple
 
 
 input_file = """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -36,26 +38,59 @@ cleared_areas = [(x[0], mh_dist_abs(x[0], x[1])) for x in inputs]
 
 beacons = set((x[1] for x in inputs))
 
-pprint(inputs)
-pprint(cleared_areas)
-pprint(beacons)
+# pprint(inputs)
+# pprint(cleared_areas)
+# pprint(beacons)
 
-result = 0
-y = 2_000_000
-x_range = range(-3_000_000, 5_000_000)
-# y = 9
-# x_range = range(-10, 40)
-for x in x_range:
-    covered = False
-    if complex(x, y) in beacons:
-        continue
+# inclusive range
+Range = namedtuple("Range", "min, max")
+
+
+def range_intersect(a: Range, b: Range):
+    if a.max < b.min or b.max < a.min:
+        return None
+    return Range(min(a.min, b.min), max(a.max, b.max))
+
+
+def find_covered_points_in_row(y):
+    ranges: List[Range] = []
+    # x_range = range(-3_000_000, 5_000_000)
+    # y = 10
     for sensor, distance in cleared_areas:
-        if mh_dist_abs(complex(x, y), sensor) <= distance:
-            covered = True
-            break
-    if covered:
-        result += 1
+        y_dist = round(abs(sensor.imag - y))
+        remaining_dist = distance - y_dist
+        if remaining_dist < 0:
+            continue
+        sensor_x = round(sensor.real)
+        ranges.append(Range(sensor_x - remaining_dist, sensor_x + remaining_dist))
 
-print(result)
+    # print(sorted(ranges))
 
+    merged_ranges = []
+    for r in sorted(ranges):
+        to_remove = []
+        intersected = False
+        for i in range(len(merged_ranges)):
+            intersected = range_intersect(r, merged_ranges[i])
+            if intersected:
+                to_remove.append(merged_ranges[i])
+                merged_ranges.append(intersected)
+        if not intersected:
+            merged_ranges.append(r)
+        for tr in to_remove:
+            merged_ranges.remove(tr)
+        # print(merged_ranges)
+
+    result = 0
+    for mr in merged_ranges:
+        result += mr.max - mr.min + 1
+    for b in beacons:
+        for r in merged_ranges:
+            if b.imag == y and b.real >= r.min and b.real <= r.max:
+                # print(f"skipping beacon at {b=}")
+                result -= 1
+    return result
+
+
+print(find_covered_points_in_row(2_000_000))
 # pprint(mh_dist_abs(8 + 7j, 2 + 10j))
