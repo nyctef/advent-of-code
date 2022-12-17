f930416229e701d572e47e1e9ed75351d0b1f496
Nyctef <nyctef@nyctef.com>
2022-12-17 09:54:30 +0000

    day 17 wip

--- day_17.py
+++ day_17.py
@@ -30,7 +30,11 @@ shapes: list[Shape] = [
     # ..#
     # ..#
     # ###
-    Shape([2 + 0j, 2 + 1j, 0 + 2j, 1 + 2j, 2 + 2j]),
+    # Shape([2 + 0j, 2 + 1j, 0 + 2j, 1 + 2j, 2 + 2j]),
+    # ###
+    # ..#
+    # ..#
+    Shape([0 + 0j, 1 + 0j, 2 + 0j, 2 + 1j, 2 + 2j]),
     # #
     # #
     # #
@@ -65,29 +69,70 @@ def read_input(name: str):
 
 
 def write_shape_to_chamber(chamber: Chamber, shape: Shape, position: complex):
+    # print(f"writing shape to chamber at {position=}")
     for point in shape.points:
         line = round((point + position).imag)
         col = round((point + position).real)
+        # print(f"setting chamber {line=} {col=}")
         chamber.rows[line][col] = "#"
+
+
+def try_move(shape: Shape, pos: complex, chamber: Chamber):
+    for p in shape.points:
+        translated_pos = p + pos
+        if translated_pos.real < 0:
+            # print("hit left side")
+            return None
+        if translated_pos.real >= 7:
+            # print("hit right side")
+            return None
+        if translated_pos.imag < 0:
+            # print("hit bottom")
+            return None
+        if chamber.rows[round(translated_pos.imag)][round(translated_pos.real)] != " ":
+            # print("hit another shape")
+            return None
+        # todo: check for intersection with other shapes in chamber
+    return pos
 
 
 def main():
     input_file = read_input("example")
+    jet_index = 0
     jets = [1 if c == ">" else -1 for c in input_file]
-    # print(jets)
 
     chamber = Chamber()
     next_shape_index = 0
     dropped_shapes_counter = 0
-    while dropped_shapes_counter < 1:
+    while dropped_shapes_counter < 10:
         next_shape = shapes[next_shape_index]
 
-        shape_position = 2 + chamber.next_rock_row * 1j
+        pos = 2 + chamber.next_rock_row * 1j
 
-        # chamber.next_rock_row += 1
+        while True:
+            pos_from_jet = pos + jets[jet_index]
+            jet_index = jet_index % len(jets)
+            # print(f"trying to apply jet {pos=} {pos_from_jet=}")
+            jet_pos = try_move(next_shape, pos_from_jet, chamber)
+            jet_pos = jet_pos if jet_pos is not None else pos
 
-        write_shape_to_chamber(chamber, next_shape, shape_position)
-        draw_chamber(chamber, 7)
+            # print(f"trying to apply gravity {jet_pos=} {jet_pos - 1j=}")
+            down_pos = try_move(next_shape, jet_pos - 1j, chamber)
+            if down_pos is None:
+                # hit something after trying to move down - shape comes to a rest
+                pos = jet_pos
+                break
+            else:
+                # otherwise loop
+                pos = down_pos
+
+        write_shape_to_chamber(chamber, next_shape, pos)
+        draw_chamber(chamber, chamber.next_rock_row)
+
+        while chamber.rows[chamber.next_rock_row - 3] != [" "] * 7:
+            # print("bumping up next rock row")
+            chamber.next_rock_row += 1
+
         next_shape_index = (next_shape_index + 1) % len(shapes)
         dropped_shapes_counter += 1
 
