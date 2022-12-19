4451ed0952110e2b61251f700e3f1fad11ff5223
Nyctef <nyctef@nyctef.com>
2022-12-19 08:15:51 +0000

    day 19 wip

--- day_19.py
+++ day_19.py
@@ -83,11 +83,111 @@ class SearchStep(NamedTuple):
     obsidian_robot_count: int
     geode_robot_count: int
 
+    def all_greater(self, other: "SearchStep"):
+        return (
+            self.ore_count > other.ore_count
+            and self.clay_count > other.clay_count
+            and self.obsidian_count > other.obsidian_count
+            and self.geode_count > other.geode_count
+            and self.ore_robot_count > other.ore_robot_count
+            and self.clay_robot_count > other.clay_robot_count
+            and self.obsidian_robot_count > other.obsidian_robot_count
+            and self.geode_robot_count > other.geode_robot_count
+        )
+
+    def simulate_minute(self):
+        return self._replace(
+            after_minute=self.after_minute + 1,
+            ore_count=self.ore_count + self.ore_robot_count,
+            clay_count=self.clay_count + self.clay_robot_count,
+            obsidian_count=self.obsidian_count + self.obsidian_robot_count,
+            geode_count=self.geode_count + self.geode_robot_count,
+        )
+
+    def can_buy_ore_robot(self, bp: Blueprint):
+        return self.ore_count >= bp.ore_robot_ore_cost
+
+    def buy_ore_robot(self, bp: Blueprint):
+        return self._replace(
+            ore_count=self.ore_count - bp.ore_robot_ore_cost,
+            ore_robot_count=self.ore_robot_count + 1,
+        )
+
+    def can_buy_clay_robot(self, bp: Blueprint):
+        return self.ore_count >= bp.clay_robot_ore_cost
+
+    def buy_clay_robot(self, bp: Blueprint):
+        return self._replace(
+            ore_count=self.ore_count - bp.clay_robot_ore_cost,
+            clay_robot_count=self.clay_robot_count + 1,
+        )
+
+    def can_buy_obsidian_robot(self, bp: Blueprint):
+        return (
+            self.ore_count >= bp.obsidian_robot_ore_cost
+            and self.clay_count >= bp.obsidian_robot_clay_cost
+        )
+
+    def buy_obsidian_robot(self, bp: Blueprint):
+        return self._replace(
+            ore_count=self.ore_count - bp.obsidian_robot_ore_cost,
+            clay_count=self.clay_count - bp.obsidian_robot_clay_cost,
+            obsidian_robot_count=self.obsidian_robot_count + 1,
+        )
+
+    def can_buy_geode_robot(self, bp: Blueprint):
+        return (
+            self.ore_count >= bp.geode_robot_ore_cost
+            and self.obsidian_count >= bp.geode_robot_obsidian_cost
+        )
+
+    def buy_geode_robot(self, bp: Blueprint):
+        return self._replace(
+            ore_count=self.ore_count - bp.geode_robot_ore_cost,
+            obsidian_count=self.obsidian_count - bp.geode_robot_obsidian_cost,
+            geode_robot_count=self.geode_robot_count + 1,
+        )
+
+
+def simulate(blueprint: Blueprint, total_time: int):
+    min1 = SearchStep(1, 1, 0, 0, 0, 1, 0, 0, 0)
+    best_after_each_min = [min1] * (total_time + 1)
+
+    q: List[SearchStep] = []
+    q.append(min1)
+    while q:
+        n = q.pop()
+        pprint(n)
+        if n.all_greater(best_after_each_min[n.after_minute]):
+            print(f"found a new best for min {n}")
+            best_after_each_min[n.after_minute] = n
+
+        if best_after_each_min[n.after_minute].all_greater(n):
+            # we're in a strictly worse state, so this branch isn't worth considering
+            print("halting early")
+            continue
+
+        if n.after_minute == total_time:
+            print("done")
+            continue
+
+        # one option is always to do nothing
+        n2 = n.simulate_minute()
+        q.append(n2)
+
+        # note checking whether we can buy at the start of the minute, but actually buying after the minute
+        if n.can_buy_clay_robot(blueprint):
+            q.append(n2.buy_clay_robot(blueprint))
+
+    return best_after_each_min
+
 
 def main():
     input = read_input("example")
     parsed = parse_input(input)
-    pprint(parsed)
+    bp0 = parsed[0]
+    scores = simulate(bp0, 5)
+    pprint(scores)
 
 
 if __name__ == "__main__":
