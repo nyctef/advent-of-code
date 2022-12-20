0a79b2000e9c9205e7b0844e76184297722f3b92
Nyctef <nyctef@nyctef.com>
2022-12-20 06:56:00 +0000

    extract a method for testing

--- day_20.py
+++ day_20.py
@@ -31,63 +31,67 @@ def nothing(*args):
 log = nothing
 
 
+def do_mix_step(step: int, parsed: list):
+    log(f"step {step+1}")
+    i, to_move = next(((i, x) for i, x in enumerate(parsed) if x[0] == step))
+    log(f"moving {to_move[1]} from index {i}")
+    where_to_move = to_move[1]
+
+    if where_to_move == 0:
+        print(f"{step=} {to_move=} ignoring zero")
+        return
+    if where_to_move % len(parsed) == 0:
+        print(f"{step=} {to_move=} ignoring loop?")
+        return
+    to_move_to = (i + where_to_move) % len(parsed)
+    target_item = parsed[to_move_to]
+    log(f"{to_move[1]=} wants to end up where {target_item[1]} currently is")
+    direction = sign(where_to_move)
+
+    log(f"we remove the item we're moving first")
+    parsed.remove(to_move)
+    log(f"then find where our target item has ended up")
+    try:
+        new_i = next(i for i, e in enumerate(parsed) if e == target_item)
+    except StopIteration:
+        print(f"{step=} failed to find {target_item} in list")
+        raise
+    log(f"our target item ended up at index {new_i}")
+    # list.insert takes the index of the item to be ahead of,
+    # so since positive moves want to go after their target item
+    # we need to add one here to be inserted before the next element
+    direction = 1 if direction > 0 else 0
+    log(f"so we're going to be at {direction=} -> {new_i + direction=}")
+    updated_loc = new_i + direction
+    if updated_loc == 0:
+        # if we're trying to replace the item at index zero, that item stays where it is:
+        # we end up looping to the end of the list
+        updated_loc = len(parsed)
+    if updated_loc == len(parsed):
+        # guess: maybe the opposite is true as well?
+        # TODO: this one hasn't been proven
+        updated_loc = 0
+
+    # if abs(where_to_move) >= len(parsed):
+    #     print(
+    #         f"{step=} moving {to_move=} from current index {i} to {to_move_to=} replacing {target_item=} {direction=} {updated_loc=}"
+    #     )
+    parsed.insert(updated_loc, to_move)
+
+    # print(parsed)
+    log([x[1] for x in parsed])
+    log()
+
+
 def main():
-    input_file = read_input("puzzle")
+    input_file = read_input("example")
     parsed = [int(x) for x in input_file.splitlines()]
     parsed = list(enumerate(parsed))
     log([x[1] for x in parsed])
     print(len(parsed))
 
     for step in range(len(parsed)):
-        log(f"step {step+1}")
-        i, to_move = next(((i, x) for i, x in enumerate(parsed) if x[0] == step))
-        log(f"moving {to_move[1]} from index {i}")
-        where_to_move = to_move[1]
-
-        if where_to_move == 0:
-            print(f"{step=} {to_move=} ignoring zero")
-            continue
-        if where_to_move % len(parsed) == 0:
-            print(f"{step=} {to_move=} ignoring loop?")
-            continue
-        to_move_to = (i + where_to_move) % len(parsed)
-        target_item = parsed[to_move_to]
-        log(f"{to_move[1]=} wants to end up where {target_item[1]} currently is")
-        direction = sign(where_to_move)
-
-        log(f"we remove the item we're moving first")
-        parsed.remove(to_move)
-        log(f"then find where our target item has ended up")
-        try:
-            new_i = next(i for i, e in enumerate(parsed) if e == target_item)
-        except StopIteration:
-            print(f"{step=} failed to find {target_item} in list")
-            raise
-        log(f"our target item ended up at index {new_i}")
-        # list.insert takes the index of the item to be ahead of,
-        # so since positive moves want to go after their target item
-        # we need to add one here to be inserted before the next element
-        direction = 1 if direction > 0 else 0
-        log(f"so we're going to be at {direction=} -> {new_i + direction=}")
-        updated_loc = new_i + direction
-        if updated_loc == 0:
-            # if we're trying to replace the item at index zero, that item stays where it is:
-            # we end up looping to the end of the list
-            updated_loc = len(parsed)
-        if updated_loc == len(parsed):
-            # guess: maybe the opposite is true as well?
-            # TODO: this one hasn't been proven
-            updated_loc = 0
-
-        # if abs(where_to_move) >= len(parsed):
-        #     print(
-        #         f"{step=} moving {to_move=} from current index {i} to {to_move_to=} replacing {target_item=} {direction=} {updated_loc=}"
-        #     )
-        parsed.insert(updated_loc, to_move)
-
-        # print(parsed)
-        log([x[1] for x in parsed])
-        log()
+        do_mix_step(step, parsed)
 
     log([x[1] for x in parsed])
     zero_index, zero = next((i, x) for i, x in enumerate(parsed) if x[1] == 0)
