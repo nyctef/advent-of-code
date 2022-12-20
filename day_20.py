f0e0a823d3a46daa444963b8e5135595f74840d0
Nyctef <nyctef@nyctef.com>
2022-12-20 06:26:55 +0000

    day 20 wip

--- day_20.py
+++ day_20.py
@@ -24,34 +24,57 @@ def sign(x: int):
     return (x > 0) - (x < 0)
 
 
+def nothing(*args):
+    pass
+
+
+log = nothing
+
+
 def main():
-    input_file = read_input("example")
+    input_file = read_input("puzzle")
     parsed = [int(x) for x in input_file.splitlines()]
     parsed = list(enumerate(parsed))
+    log([x[1] for x in parsed])
 
     for step in range(len(parsed)):
-        # print(f"{step=}")
+        log(f"step {step+1}")
         i, to_move = next(((i, x) for i, x in enumerate(parsed) if x[0] == step))
-        # print(f"moving {to_move[1]} from index {i}")
+        log(f"moving {to_move[1]} from index {i}")
         where_to_move = to_move[1]
-        # off-by-one hacks
-        if i + where_to_move <= 0:
-            where_to_move -= 1
-        if i + where_to_move >= len(parsed):
-            where_to_move += 1
-        direction = sign(where_to_move)
-        # print(f"moving {to_move[1]} {where_to_move=} {direction=}")
-        current_i = i
+
+        if where_to_move == 0:
+            log(f"ignoring zero")
+            continue
         to_move_to = (i + where_to_move) % len(parsed)
+        target_item = parsed[to_move_to]
+        log(f"{to_move[1]=} wants to end up where {target_item[1]} currently is")
+        direction = sign(where_to_move)
+        # log(f"moving {to_move[1]} {where_to_move=} {direction=}")
+        current_i = i
 
-        del parsed[current_i]
-        parsed.insert(to_move_to, to_move)
+        # del parsed[current_i]
+        log(f"we remove the item we're moving first")
+        parsed.remove(to_move)
+        log(f"then find where our target item has ended up")
+        try:
+            new_i = next(i for i, e in enumerate(parsed) if e == target_item)
+        except StopIteration:
+            print(f"{step=} failed to find {target_item} in list")
+            raise
+        log(f"our new item ended up at index {new_i}")
+        direction = 1 if direction > 0 else 0
+        log(f"so we're going to be at {direction=} -> {new_i + direction=}")
+        updated_loc = new_i + direction
+        if updated_loc == 0:
+            updated_loc = len(parsed)
+        parsed.insert(updated_loc, to_move)
 
-        # pprint(parsed)
-        # pprint([x[1] for x in parsed])
-        # print()
+        # print(parsed)
+        log([x[1] for x in parsed])
+        log()
 
-    print([x[1] for x in parsed])
+    log([x[1] for x in parsed])
     zero_index, zero = next((i, x) for i, x in enumerate(parsed) if x[1] == 0)
     x = parsed[(zero_index + 1000) % len(parsed)][1]
     y = parsed[(zero_index + 2000) % len(parsed)][1]
