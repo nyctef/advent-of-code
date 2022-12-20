a5f4e46177c6896f8e4b0fb0c0954414450f45ed
Nyctef <nyctef@nyctef.com>
2022-12-20 07:42:21 +0000

    realising there's an inconsistency

--- day_20.py
+++ day_20.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from pprint import pprint
+from typing import Any
 
 
 def read_input(name: str):
@@ -28,26 +29,21 @@ def nothing(*args):
     pass
 
 
-log = nothing
-
-
-def do_mix_step(step: int, parsed: list):
+def do_mix_step(step: int, parsed: list, log: Any):
     log(f"step {step+1}")
     i, to_move = next(((i, x) for i, x in enumerate(parsed) if x[0] == step))
     log(f"moving {to_move[1]} from index {i}")
     where_to_move = to_move[1]
 
     if where_to_move == 0:
-        print(f"{step=} {to_move=} ignoring zero")
+        log(f"{step=} {to_move=} ignoring zero")
         return
     if where_to_move % len(parsed) == 0:
-        print(f"{step=} {to_move=} ignoring loop?")
+        log(f"{step=} {to_move=} ignoring loop?")
         return
     to_move_to = (i + where_to_move) % len(parsed)
     if abs(to_move_to) < 5:
-        print(
-            f"{step=} near-boundary condition moving {to_move} at {i} to {to_move_to}"
-        )
+        log(f"{step=} near-boundary condition moving {to_move} at {i} to {to_move_to}")
     target_item = parsed[to_move_to]
     log(f"{to_move[1]=} wants to end up where {target_item[1]} currently is")
 
@@ -63,12 +59,18 @@ def do_mix_step(step: int, parsed: list):
     # list.insert takes the index of the item to be ahead of,
     # so since positive moves want to go after their target item
     # we need to add one here to be inserted before the next element
-    insert_fixup = 1 if where_to_move > 0 else 0
+    insert_fixup = 0
+    if where_to_move > 0 and new_i >= i:
+        insert_fixup = 1
+    if where_to_move < 0 and new_i >= i:
+        insert_fixup = 1
     log(f"so we're going to be at {insert_fixup=} -> {new_i + insert_fixup=}")
     updated_loc = new_i + insert_fixup
     if where_to_move < 0 and updated_loc == 0:
         # if we're trying to replace the item at index zero, that item stays where it is:
         # we end up looping to the end of the list
+        # realised this doesn't really matter in the end (it just makes the output look more like the examples)
+        # since we should be treating the list as a ring anyway
         updated_loc = len(parsed)
     # if updated_loc == len(parsed):
     #     # guess: maybe the opposite is true as well?
@@ -82,19 +84,19 @@ def do_mix_step(step: int, parsed: list):
     parsed.insert(updated_loc, to_move)
 
     # print(parsed)
-    log([x[1] for x in parsed])
-    log()
+    # log([x[1] for x in parsed])
+    # log()
 
 
 def main(input_name):
     input_file = read_input(input_name)
     parsed = [int(x) for x in input_file.splitlines()]
     parsed = list(enumerate(parsed))
-    log([x[1] for x in parsed])
+    nothing([x[1] for x in parsed])
     print(len(parsed))
 
     for step in range(len(parsed)):
-        do_mix_step(step, parsed)
+        do_mix_step(step, parsed, nothing)
 
     if len(parsed) < 20:
         print([x[1] for x in parsed])
@@ -106,14 +108,28 @@ def main(input_name):
     print(x + y + z)
 
 
-def tests():
-    print(f"test 1")
+def test_1():
+    print(f"test 1 ----------------")
     l1 = [0, 1, 2, 3, "x", 5, 6, 7, 8, 9]
     for x in range(12):
+        # for x in [2, 6]:
         l = list(enumerate([x if a == "x" else a for a in l1]))
         print(f" testing with {x=}")
         print([x[1] for x in l])
-        do_mix_step(4, l)
+        do_mix_step(4, l, nothing)
+        print([x[1] for x in l])
+        print()
+
+
+def test_2():
+    print(f"test 2 ----------------")
+    l1 = [0, 1, 2, 3, "x", 5, 6, 7, 8, 9]
+    for x in range(-12, 0):
+        # for x in [-9, -2]:
+        l = list(enumerate([x if a == "x" else a for a in l1]))
+        print(f" testing with {x=}")
+        print([x[1] for x in l])
+        do_mix_step(4, l, print if x in () else nothing)
         print([x[1] for x in l])
         print()
 
@@ -121,4 +137,5 @@ def tests():
 if __name__ == "__main__":
     main("example")
     # main("puzzle")
-    tests()
+    test_1()
+    test_2()
