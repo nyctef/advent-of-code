7c3da26d359db254ba67ea9fdd1ca1d24c958080
Nyctef <nyctef@nyctef.com>
2022-12-20 17:24:14 +0000

    it was simple all along

--- day_20.py
+++ day_20.py
@@ -32,45 +32,11 @@ def nothing(*args):
 def do_mix_step(step: int, parsed: list, log: Any):
     log(f"step {step+1}")
     current_i, to_move = next(((i, x) for i, x in enumerate(parsed) if x[0] == step))
-    log(f"moving {to_move[1]} from index {current_i}")
-    move_distance = to_move[1]
-    s = sign(move_distance)
-    if move_distance == 0:
-        log(f"zero doesn't move")
-        return
-    if abs(move_distance) % (len(parsed) - 1) == 0:
-        log(f"moving n-1 positions seems to be a loop")
-        return
-    bound_i1 = (current_i + move_distance) % (len(parsed))
-    bound_i2 = (current_i + move_distance + sign(move_distance)) % (len(parsed))
-    bound_i1, bound_i2 = min(bound_i1, bound_i2), max(bound_i1, bound_i2)
-    log(f"{current_i=} {move_distance=} {len(parsed) - 1=} {bound_i1=} {bound_i2=}")
-    if bound_i1 == current_i:
-        bound_i1 = (bound_i1 - 1) % len(parsed)
-    if bound_i2 == current_i:
-        bound_i2 = (bound_i2 + 1) % len(parsed)
-    bound_1 = parsed[bound_i1][1]
-    bound_2 = parsed[bound_i2][1]
-    log(f"{to_move[1]} moves between {bound_1} and {bound_2}")
-    log("removing to_move")
-    # log("finding where our bounds ended up")
-    # bound_i1, _ = next((i, e) for i, e in enumerate(parsed) if e[1] == bound_1)
-    # bound_i2, _ = next((i, e) for i, e in enumerate(parsed) if e[1] == bound_2)
 
-    min_index = min(bound_i1, bound_i2)
-    max_index = max(bound_i1, bound_i2)
-    log(f"{bound_i1=} {bound_i2=} {min_index=} {max_index=}")
-    if min_index == 0 and max_index == len(parsed) - 1:
-        log(f"sticking it at the end of the list")
-        insert_location = len(parsed)
-    else:
-        insert_location = max_index
-
-    parsed.insert(insert_location, to_move)
-    log(f"after inserting: {parsed}")
-    if insert_location <= current_i:
-        current_i += 1
+    distance = to_move[1]
     del parsed[current_i]
+    new_loc = (current_i + distance) % len(parsed)
+    parsed.insert(new_loc, to_move)
 
 
 def main(input_name, log, multiplier, rounds):
@@ -106,11 +72,10 @@ def test_1():
     l1 = [0, 1, 2, 3, "x", 5, 6, 7, 8, 9]
     for x in range(24):
         # for x in [2, 6]:
-        l = list(enumerate([x if a == "x" else a for a in l1 if a != x]))
+        l = list(enumerate([x if a == "x" else a for a in l1]))
         print(f" testing with {x=}")
         print([x[1] for x in l])
-        step_index = next(i for i, e in enumerate(l) if e[1] == x)
-        do_mix_step(step_index, l, print if x in (5,) else nothing)
+        do_mix_step(4, l, print if x in (5,) else nothing)
         print([x[1] for x in l])
         print()
 
@@ -120,7 +85,7 @@ def test_2():
     l1 = [0, 1, 2, 3, "x", 5, 6, 7, 8, 9]
     for x in range(-24, 0):
         # for x in [-9, -2]:
-        l = list(enumerate([x if a == "x" else a for a in l1 if a != x]))
+        l = list(enumerate([x if a == "x" else a for a in l1]))
         print(f" testing with {x=}")
         print([x[1] for x in l])
         do_mix_step(4, l, print if x in (-10,) else nothing)
@@ -129,8 +94,8 @@ def test_2():
 
 
 if __name__ == "__main__":
-    main("example", print, 1, 1)
-    main("example", print, 811589153, 10)
-    # main("puzzle", nothing)
+    # main("example", print, 1, 1)
+    # main("example", print, 811589153, 10)
+    main("puzzle", nothing, 811589153, 10)
     # test_1()
     # test_2()
