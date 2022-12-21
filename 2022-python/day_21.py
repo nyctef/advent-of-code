c576508d8bb38933101b0d7e2ca6ec5bd003e79c
Nyctef <nyctef@nyctef.com>
2022-12-21 06:03:57 +0000

    day 21 part 2 first attempt: use z3?

--- 2022-python/day_21.py
+++ 2022-python/day_21.py
@@ -1,7 +1,9 @@
+from collections import defaultdict
 from itertools import chain
 from pathlib import Path
 from pprint import pprint
 import re
+import z3
 
 
 def read_input_file(name: str):
@@ -30,7 +32,7 @@ hmdt: 32
 
 
 def parse_input(input: str):
-    result = {}
+    result: dict[str, str] = {}
     for line in input.splitlines():
         left, right = line.split(":")
         result[left] = right.strip()
@@ -41,30 +43,59 @@ def is_digits(s: str):
     return re.match(r"^\d+$", s)
 
 
+varnum = 1
+
+
+def new_var():
+    global varnum
+    res = z3.Int(f"{varnum}")
+    varnum += 1
+    return res
+
+
 def main():
     input = read_input_file("puzzle")
     lookup = parse_input(input)
-    resolved = {}
 
-    while lookup:
-        for l, r in lookup.items():
-            if is_digits(r):
-                resolved[l] = r
-                continue
+    vars = defaultdict(new_var)
+    humn = vars["humn"]
+
+    s = z3.Solver()
+
+    # s.add(humn == z3.IntVal(42))
+    for l, r in lookup.items():
+        if l == "humn":
+            # we're told to ignore this one for part 2
+            continue
+        var = vars[l]
+        if is_digits(r):
+            val = int(r)
+            s.add(var == z3.IntVal(val))
+        else:
             m = re.match(r"^(\w{4}).(.).(\w{4})$", r)
             assert m is not None
             left, op, right = m.group(1), m.group(2), m.group(3)
-            rleft = resolved.get(left, None)
-            rright = resolved.get(right, None)
-            if rleft is not None and rright is not None:
-                resolved[l] = eval(f"{rleft} {op} {rright}")
+            lvar = vars[left]
+            rvar = vars[right]
+            if l == "root":
+                s.add(lvar == rvar)
+                continue
+            match op:
+                case "+":
+                    s.add(var == lvar + rvar)
+                case "-":
+                    s.add(var == lvar - rvar)
+                case "*":
+                    s.add(var == lvar * rvar)
+                case "/":
+                    s.add(var == lvar / rvar)
+                case other:
+                    raise Exception(f"unknown op {other}")
 
-        for l in resolved.keys():
-            lookup.pop(l, None)
-
-    pprint(resolved)
-    pprint(lookup)
-    print(resolved["root"])
+    print(s.assertions())
+    print(s.check())
+    m = s.model()
+    print(f"{m[humn]=}")
 
 
 """
