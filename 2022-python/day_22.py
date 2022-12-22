b8f749ff7319e8d660174722641844ac12b8b931
Nyctef <nyctef@nyctef.com>
2022-12-22 11:46:48 +0000

    day 22 wip

--- 2022-python/day_22.py
+++ 2022-python/day_22.py
@@ -105,8 +105,7 @@ def get_point_on_new_face(
     old_point: Point, old_direction: int, new_direction: int, side_length: int
 ):
     if not (
-        (old_point[0] in (0, side_length - 1))
-        and (old_point[1] in (0, side_length - 1))
+        (old_point[0] in (0, side_length - 1)) or (old_point[1] in (0, side_length - 1))
     ):
         raise Exception(
             f"bad point {old_point=} {old_direction=} {new_direction=} {side_length=}"
@@ -117,48 +116,48 @@ def get_point_on_new_face(
     # straight lines
     if old_direction == DOWN and new_direction == DOWN:
         # appear at the top of the next face
-        return (0, old_column)
+        return Point(0, old_column)
     if old_direction == UP and new_direction == UP:
         # appear at the bottom of the next face
-        return (side_length - 1, old_column)
+        return Point(side_length - 1, old_column)
     if old_direction == RIGHT and new_direction == RIGHT:
         # appear at the left (first column) of the next face
-        return (old_row, 0)
+        return Point(old_row, 0)
     if old_direction == LEFT and new_direction == LEFT:
         # appear at the right (last column) of the next face
-        return (old_row, side_length - 1)
+        return Point(old_row, side_length - 1)
 
     # right turns
     if old_direction == RIGHT and new_direction == DOWN:
         # starting from the rhs of one side, ending up on the top row of the next
         # closest corner is the bottom right of the original side which maps to the top left of the next
-        return (0, side_length - old_row)
+        return Point(0, side_length - old_row)
     if old_direction == DOWN and new_direction == LEFT:
-        return (old_column, side_length - 1)
+        return Point(old_column, side_length - 1)
     if old_direction == LEFT and new_direction == UP:
-        return (side_length - 1, side_length - old_row)
+        return Point(side_length - 1, side_length - old_row)
     if old_direction == UP and new_direction == RIGHT:
-        return (old_column, 0)
+        return Point(old_column, 0)
 
     # left turns
     if old_direction == RIGHT and new_direction == UP:
-        return (side_length - 1, old_row)
+        return Point(side_length - 1, old_row)
     if old_direction == UP and new_direction == LEFT:
-        return (side_length - old_column, side_length - 1)
+        return Point(side_length - old_column, side_length - 1)
     if old_direction == LEFT and new_direction == DOWN:
-        return (0, old_row)
+        return Point(0, old_row)
     if old_direction == DOWN and new_direction == RIGHT:
-        return (side_length - old_column, 0)
+        return Point(side_length - old_column, 0)
 
     # flips
     if (old_direction == RIGHT and new_direction == LEFT) or (
         old_direction == LEFT and new_direction == RIGHT
     ):
-        return (side_length - old_row - 1, old_column)
+        return Point(side_length - old_row - 1, old_column)
     if (old_direction == DOWN and new_direction == UP) or (
         old_direction == UP and new_direction == DOWN
     ):
-        return (old_row, side_length - old_column - 1)
+        return Point(old_row, side_length - old_column - 1)
 
     raise Exception(
         f"unhandled directions {old_point=} {old_direction=} {new_direction=} {side_length=}"
@@ -169,8 +168,7 @@ def add(a: Point, b: Point):
     return Point(a[0] + b[0], a[1] + b[1])
 
 
-def run_path(parsed):
-    grid, instructions = parsed
+def run_path(grid: list[CubeSide], instructions: list[str], map: list[MapEntry]):
     # right, down, left, up
     # remember row, column
     dirs: list[Point] = [Point(0, 1), Point(1, 0), Point(0, -1), Point(-1, 0)]
@@ -185,39 +183,15 @@ def run_path(parsed):
         nonlocal current_dir
         current_dir = (current_dir - 1) % len(dirs)
 
-    def wrap(pos: Point):
-        r, c = pos
-        if r < 0:
-            r = len(grid) - 1
-        if r >= len(grid):
-            r = 0
-        if c < 0:
-            c = len(grid[0]) - 1
-        if c >= len(grid[0]):
-            c = 0
-        return Point(r, c)
+    current_grid = grid[0]
 
-    def char_at(p: Point):
-        return grid[p[0]][p[1]]
-
-    def wrap_whitespace(p: Point, dir: Point):
-        count = 0
-        while char_at(p) == " ":
-            count += 1
-            if count >= 999_999:
-                raise Exception(f"loop! {p=} {dir=}")
-
-            p = wrap(add(p, dir))
-        return p
-
-    first_line = grid[0]
-    initial_column = next(i for i, e in enumerate(first_line) if e != " ")
-    print(f"{initial_column=}")
+    def char_at(cs: CubeSide, p: Point):
+        return cs.chars[p[0]][p[1]]
 
     # row, column (0-offset, but final result will need to be 1-offset so remember that)
-    current_position = Point(0, initial_column)
+    current_position = Point(0, 0)
     for instr in instructions:
-        print(f"next {instr=} {current_position=}")
+        print(f"next {instr=} {current_position=} {current_grid.id=}")
         if instr == "L":
             turn_left()
         elif instr == "R":
@@ -226,18 +200,49 @@ def run_path(parsed):
             steps = int(instr)
             for _ in range(steps):
                 next_position = add(current_position, dirs[current_dir])
-                next_position = wrap(next_position)
-                next_char = char_at(next_position)
+                next_grid = current_grid
+                next_dir = current_dir
+                if (
+                    next_position.r < 0
+                    or next_position.r >= current_grid.side_width
+                    or next_position.c < 0
+                    or next_position.c >= current_grid.side_width
+                ):
+                    mp = next(p for p in map if p.id == current_grid.id)
+                    next_grid_id = [
+                        mp.right_page,
+                        mp.down_page,
+                        mp.left_page,
+                        mp.up_page,
+                    ][current_dir]
+                    next_dir = [
+                        mp.dir_after_right,
+                        mp.dir_after_down,
+                        mp.dir_after_left,
+                        mp.dir_after_up,
+                    ][current_dir]
+                    next_grid = next(g for g in grid if g.id == next_grid_id)
+                    try:
+                        next_position = get_point_on_new_face(
+                            current_position,
+                            current_dir,
+                            next_dir,
+                            current_grid.side_width,
+                        )
+                    except Exception as e:
+                        raise Exception(
+                            f"{current_position=} {next_position=} {current_dir=} {next_dir=} {current_grid.id=} {next_grid.id=}"
+                        ) from e
 
-                if next_char == " ":
-                    next_position = wrap_whitespace(next_position, dirs[current_dir])
-                    next_char = char_at(next_position)
+                next_char = char_at(next_grid, next_position)
 
                 if next_char == ".":
                     current_position = next_position
+                    current_grid = next_grid
+                    current_dir = next_dir
                 elif next_char == "#":
                     break
-                elif next_char == " ":
+                else:
                     raise Exception(next_position)
     print(
         (1000 * (current_position[0] + 1))
@@ -247,10 +252,12 @@ def run_path(parsed):
 
 
 def main():
-    input_file, side_width = read_input("example")
-    parsed = parse_input(input_file, side_width)
-    pprint(parsed)
-    # run_path(parsed)
+    input_name = "example"
+    input_file, side_width = read_input(input_name)
+    grid, instructions = parse_input(input_file, side_width)
+    map = get_map(input_name)
+    # pprint(parsed)
+    run_path(grid, instructions, map)
 
 
 main()
