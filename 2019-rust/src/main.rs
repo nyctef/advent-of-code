f908048d05ea6ba722ea07aec405480b9b1a3590
Nyctef <nyctef@nyctef.com>
2022-12-24 21:01:06 +0000

    thinking about making a common CLI for rust 2019

---
+++ 2019-rust/src/main.rs
@@ -0,0 +1,10 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let usage = "Usage: [exe] [day] [puzzle|example|input-name]";
+    let _day: u8 = std::env::args()
+        .nth(1)
+        .ok_or(usage)?
+        .parse()
+        .map_err(|_| usage.to_owned() + ": failed to parse day as u8")?;
+    let _path = std::env::args().nth(2).ok_or(usage)?;
+    Ok(())
+}
