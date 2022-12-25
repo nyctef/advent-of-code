37a8c0970b83e06a9c1d1abbd3a9214501d2d965
Nyctef <nyctef@nyctef.com>
2022-12-25 16:15:29 +0000

    comments and cleanup for day 25

--- 2022-python/day_25.py
+++ 2022-python/day_25.py
@@ -1,5 +1,7 @@
 from itertools import zip_longest
+from math import log
 from pathlib import Path
+from typing import Iterator
 
 
 def read_input(name: str):
@@ -25,57 +27,68 @@ def read_input(name: str):
             raise Exception(other)
 
 
-s_lookup = {"=": -2, "-": -1, "0": 0, "1": 1, "2": 2}
-lookup = {"=": 0, "-": 1, "0": 2, "1": 3, "2": 4}
-r_lookup = {v: k for k, v in lookup.items()}
+snafu_lookup = {"=": -2, "-": -1, "0": 0, "1": 1, "2": 2}
+reverse_lookup = {v: k for k, v in snafu_lookup.items()}
 
 
 def snafu_to_dec(x: str):
     total = 0
     e = 1
     for i in reversed(range(len(x))):
-        total += e * (s_lookup[x[i]])
+        total += e * (snafu_lookup[x[i]])
         e *= 5
     return total
 
 
-def conv(x: str):
-    return [lookup[i] for i in x]
+def snafu_to_dec_digits(x: str):
+    return [snafu_lookup[i] for i in x]
+
+
+def dec_digits_to_snafu(result: Iterator[int]):
+    return "".join(reverse_lookup[x] for x in result)
 
 
 def add_snafu(a: str, b: str):
-    print()
-    print(f"input: {a=} {b=}")
-    matched = list(zip_longest(reversed(conv(a)), reversed(conv(b)), fillvalue=0))
-    result = []
+    # match digits together in order from least significant to most
+    matched_digits = list(
+        zip_longest(
+            reversed(snafu_to_dec_digits(a)),
+            reversed(snafu_to_dec_digits(b)),
+            fillvalue=0,
+        )
+    )
+    # starting from the least significant digit, add values together
+    # and carry upwards if necessary
+    result: list[int] = []
     carry = 0
-    for a1, b1 in matched:
-        rem = (a1 + b1 + carry) % 5
-        carry = (a1 + b1 + carry) // 5
-        result.append(rem)
+    for a1, b1 in matched_digits:
+        n = a1 + b1 + carry
+        carry = 0
+        if n > 2:
+            n -= 5
+            carry += 1
+        elif n < -2:
+            n += 5
+            carry -= 1
+
+        result.append(n)
+
+    # if there's value left over, it becomes the new most significant digit
     if carry != 0:
         result.append(carry)
 
-    print(matched)
-    print(result)
-    as_snafu = "".join(r_lookup[x] for x in reversed(result))
-    print(
-        f"added {snafu_to_dec(a)} and {snafu_to_dec(b)} to get {snafu_to_dec(as_snafu)} ({as_snafu})"
-    )
-    return as_snafu
-
-
-def parse_input(input_file: str):
-    return input_file.splitlines()
+    # then reverse the digits back again so the most significant is first,
+    # and convert decimal digits back to a snafu string
+    return dec_digits_to_snafu(reversed(result))
 
 
 def main():
-    add_snafu("0", "1")
-    # i = parse_input(read_input("example"))
-    # total = "0"
-    # for n in i:
-    #     total = add_snafu(total, n)
-    #     print(total)
+    i = read_input("puzzle").splitlines()
+    total = "0"
+    for n in i:
+        total = add_snafu(total, n)
+    print(total)
+    assert snafu_to_dec(total) == sum(snafu_to_dec(x) for x in i)
 
 
 main()
