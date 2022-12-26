e94a9156ddcf8b88b4a09bce4beb9770c0f2d084
Mark Jordan <Mark.Jordan@red-gate.com>
2022-12-26 17:04:32 +0000

    day 1 part 1

--- 2019-rust/src/day1.rs
+++ 2019-rust/src/day1.rs
@@ -1,14 +1,29 @@
-use std::error::Error;
+use std::{cmp::max, error::Error};
 
 use crate::aoc_util::get_input;
 
 pub fn part1() -> Result<(), Box<dyn Error>> {
     let input = get_input(2019, 1)?;
-    print!("{input}");
+    let sum = input
+        .lines()
+        .map(|x| get_mass(str::parse::<u32>(x).unwrap()))
+        .sum::<u32>();
+
+    print!("{sum}");
     Ok(())
 }
 
+fn get_mass(obj: u32) -> u32 {
+    (obj / 3).saturating_sub(2)
+}
+
+#[test]
+fn test1() {
+    assert_eq!(2, get_mass(12));
+    assert_eq!(0, get_mass(2));
+}
+
 pub fn part2() -> Result<(), Box<dyn Error>> {
-    let input = get_input(2019, 1)?;
+    let _input = get_input(2019, 1)?;
     Ok(())
 }
