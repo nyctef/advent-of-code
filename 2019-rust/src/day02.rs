e0d5e927d3e9c06a1f4b3e5223ab23c1e6b51fe3
Mark Jordan <Mark.Jordan@red-gate.com>
2022-12-27 12:59:54 +0000

    day 2 part 1

--- 2019-rust/src/day02.rs
+++ 2019-rust/src/day02.rs
@@ -32,6 +32,10 @@ impl IntCode {
         self.data[position] = value;
     }
 
+    fn get_value_at(&self, position: usize) -> TInt {
+        self.data[position]
+    }
+
     fn read_op_at(&self, position: &mut usize) -> Result<Opcode> {
         let opnum = self.data[*position];
         match opnum {
@@ -44,8 +48,48 @@ impl IntCode {
                 *position += 4;
                 return Ok(op);
             }
+            2 => {
+                let op = Opcode::Mul {
+                    input_pos_1: self.data[*position + 1],
+                    input_pos_2: self.data[*position + 2],
+                    output_pos: self.data[*position + 3],
+                };
+                *position += 4;
+                return Ok(op);
+            }
+            99 => {
+                let op = Opcode::Halt;
+                *position += 1;
+                return Ok(op);
+            }
             other => todo!("unknown opcode {other}"),
         };
+    }
+
+    fn execute_op(&mut self, op: Opcode) -> bool {
+        match op {
+            Opcode::Add {
+                input_pos_1,
+                input_pos_2,
+                output_pos,
+            } => {
+                let a = self.data[input_pos_1];
+                let b = self.data[input_pos_2];
+                self.data[output_pos] = a + b;
+                return true;
+            }
+            Opcode::Mul {
+                input_pos_1,
+                input_pos_2,
+                output_pos,
+            } => {
+                let a = self.data[input_pos_1];
+                let b = self.data[input_pos_2];
+                self.data[output_pos] = a * b;
+                return true;
+            }
+            Opcode::Halt => return false,
+        }
     }
 }
 
@@ -71,8 +115,14 @@ pub fn solve() -> Result<()> {
     intcode.set_value_at_position(2, 2);
 
     let mut pc: usize = 0;
-    let op1 = intcode.read_op_at(&mut pc)?;
-    dbg!(op1, pc);
+
+    let mut running = true;
+    while running {
+        let op = intcode.read_op_at(&mut pc)?;
+        running = intcode.execute_op(op);
+    }
+
+    print!("{}", intcode.get_value_at(0));
 
     Ok(())
 }
