2ad0ad8f6fdd0268cff60610166d73152180d3ff
Nyctef <nyctef@nyctef.com>
2022-12-31 15:47:13 +0000

    make sure the input/ folder gets created if it doesn't exist

--- 2019-rust/src/aoc_util.rs
+++ 2019-rust/src/aoc_util.rs
@@ -1,15 +1,18 @@
-use color_eyre::eyre::Result;
-use std::{fs, path::Path};
+use color_eyre::eyre::{Context, Result};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 use reqwest::{blocking::Client, cookie::Jar, Url};
 
 pub fn get_input(year: u16, day: u8) -> Result<String> {
-    let cookie = fs::read_to_string("./.cookie").expect(
+    let cookie = fs::read_to_string("./.cookie").wrap_err(
         "Need to be able to read .cookie file to get AoC input: \
 make sure this file exists and you are in the correct directory",
-    );
+    )?;
 
-    let input_file_path = format!("input/day_{day}.txt");
+    let input_file_path = PathBuf::from(format!("input/day_{day}.txt"));
     if Path::new(&input_file_path).exists() {
         return Ok(fs::read_to_string(input_file_path)?);
     }
@@ -26,7 +29,9 @@ make sure this file exists and you are in the correct directory",
         .send()?
         .text()?;
 
-    fs::write(input_file_path, &puzzle_string)?;
+    fs::create_dir_all(input_file_path.parent().unwrap())
+        .wrap_err("failed to create folder for input files")?;
+    fs::write(input_file_path, &puzzle_string).wrap_err("failed to write puzzle input file")?;
 
     Ok(puzzle_string)
 }
