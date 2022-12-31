2f0a7f6ef6b46a8b58b339d803c596499846dc69
Nyctef <nyctef@nyctef.com>
2022-12-31 17:17:23 +0000

    day 13 wip

--- 2019-rust/src/day13.rs
+++ 2019-rust/src/day13.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use crate::{aoc_util::*, intcode::IntCode};
 use color_eyre::eyre::Result;
@@ -8,6 +8,7 @@ pub fn solve() -> Result<()> {
     let input = get_input(2019, 13)?;
 
     let mut machine = input.parse::<IntCode>()?;
+    machine.set_value_at_position(0, 2);
     machine.run()?;
 
     let mut output = vec![];
@@ -16,13 +17,31 @@ pub fn solve() -> Result<()> {
     }
 
     let tiles = output.iter().tuples::<(_, _, _)>().collect_vec();
-    let block_tiles = tiles
+    let tiles = tiles
         .iter()
-        .filter(|(_x, _y, t)| **t == 2)
-        .map(|(x, y, _)| (x, y))
-        .collect::<HashSet<_>>();
-    println!("{}", block_tiles.len());
+        .map(|(x, y, t)| ((**x, **y), t))
+        .collect::<HashMap<_, _>>();
+    let max_x = tiles.keys().map(|k| k.0).max().unwrap();
+    let max_y = tiles.keys().map(|k| k.1).max().unwrap();
+    dbg!(max_x, max_y);
 
+    for row in 0..=max_y {
+        for col in 0..=max_x {
+            print!(
+                "{}",
+                match tiles.get(&(col, row)) {
+                    Some(1) => "█",
+                    Some(2) => "#",
+                    Some(3) => "_",
+                    Some(4) => "o",
+                    _other => " ",
+                }
+            );
+        }
+        println!();
+    }
+
+    println!("Score: {}", tiles[&(-1, 0)]);
     Ok(())
 }
 
