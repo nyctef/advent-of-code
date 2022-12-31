bafe58715855bee3358cd078bea601d3fe5b010d
Nyctef <nyctef@nyctef.com>
2022-12-31 17:24:03 +0000

    :o

--- 2019-rust/src/day13.rs
+++ 2019-rust/src/day13.rs
@@ -1,21 +1,43 @@
-use std::collections::{HashMap, HashSet};
-
-use crate::{aoc_util::*, intcode::IntCode};
+use crate::{
+    aoc_util::*,
+    intcode::{IntCode, MachineState},
+};
 use color_eyre::eyre::Result;
 use itertools::Itertools;
+use std::time;
+use std::{collections::HashMap, thread};
 
 pub fn solve() -> Result<()> {
     let input = get_input(2019, 13)?;
 
     let mut machine = input.parse::<IntCode>()?;
     machine.set_value_at_position(0, 2);
-    machine.run()?;
+
+    loop {
+        machine.run()?;
+
+        print_game(&mut machine);
+
+        thread::sleep(time::Duration::from_millis(500));
+
+        if machine.state().1 == MachineState::Halted {
+            break;
+        } else if machine.state().1 == MachineState::AwaitingInput {
+            machine.queue_input(1);
+        }
+    }
+
+    Ok(())
+}
+
+fn print_game(machine: &mut IntCode) {
+    // https://stackoverflow.com/questions/34837011/
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 
     let mut output = vec![];
     while let Some(x) = machine.read_output() {
         output.push(x)
     }
-
     let tiles = output.iter().tuples::<(_, _, _)>().collect_vec();
     let tiles = tiles
         .iter()
@@ -24,7 +46,6 @@ pub fn solve() -> Result<()> {
     let max_x = tiles.keys().map(|k| k.0).max().unwrap();
     let max_y = tiles.keys().map(|k| k.1).max().unwrap();
     dbg!(max_x, max_y);
-
     for row in 0..=max_y {
         for col in 0..=max_x {
             print!(
@@ -40,9 +61,7 @@ pub fn solve() -> Result<()> {
         }
         println!();
     }
-
-    println!("Score: {}", tiles[&(-1, 0)]);
-    Ok(())
+    println!("Score: {:?}", tiles.get(&(-1, 0)));
 }
 
 #[test]
