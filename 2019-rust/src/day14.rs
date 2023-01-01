b18c94d01490ef1a476450c4a3d235bee28d36a8
Nyctef <nyctef@nyctef.com>
2023-01-01 13:29:02 +0000

    first complete attempt at 14

--- 2019-rust/src/day14.rs
+++ 2019-rust/src/day14.rs
@@ -9,6 +9,10 @@ use nom::{
     sequence::separated_pair,
     Finish,
 };
+use std::{
+    collections::{HashMap, VecDeque},
+    fmt::Debug,
+};
 
 pub fn solve() -> Result<()> {
     let input = get_input(2019, 14)?;
@@ -22,9 +26,50 @@ pub fn solve() -> Result<()> {
 fn solve_for(input: &str) -> Result<String> {
     let parsed = parse_input(input)?;
 
-    dbg!(parsed);
+    let lookup = parsed
+        .iter()
+        .map(|r| (&r.output.name[..], r))
+        .collect::<HashMap<_, _>>();
 
-    todo!()
+    assert!(
+        lookup.len() == parsed.len(),
+        "each item should only have one recipe"
+    );
+
+    let mut requirements: HashMap<&str, u32> = HashMap::new();
+    requirements.insert("FUEL", 1);
+    let mut steps: VecDeque<&str> = VecDeque::new();
+    steps.push_back("FUEL");
+
+    while let Some(next) = steps.pop_front() {
+        if next == "ORE" {
+            continue;
+        }
+        // in theory we should have calculated the total required sum of this ingredient by now
+        let recipe = lookup
+            .get(next)
+            .ok_or(format!("missing recipe for {}", next))
+            .map_err(Report::msg)?;
+        let needed = requirements.get(next).expect("requirements");
+        let recipe_runs = div_ceil(*needed, recipe.output.quantity);
+
+        dbg!(next, recipe, needed, recipe_runs);
+        println!("-----------------------------");
+
+        for input in &recipe.inputs {
+            *requirements.entry(&input.name).or_insert(0) += input.quantity * recipe_runs;
+            if !steps.contains(&&input.name[..]) {
+                steps.push_back(&input.name);
+            }
+        }
+    }
+
+    Ok(requirements["ORE"].to_string())
+}
+
+fn div_ceil(a: u32, b: u32) -> u32 {
+    // https://stackoverflow.com/a/72442854/895407
+    (a + b - 1) / b
 }
 
 fn parse_input(input: &str) -> Result<Vec<Recipe>> {
@@ -60,7 +105,6 @@ impl Recipe {
     }
 }
 
-#[derive(Debug)]
 struct Ingredient {
     quantity: u32,
     name: String,
@@ -71,6 +115,11 @@ impl Ingredient {
             quantity: u32::from_str_radix(q, 10).unwrap(),
             name: n.to_owned(),
         }
+    }
+}
+impl Debug for Ingredient {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.quantity, self.name)
     }
 }
 
@@ -86,6 +135,6 @@ fn test_example1() -> Result<()> {
 "###;
     let result = solve_for(input)?;
 
-    assert_eq!("expected", result);
+    assert_eq!("31", result);
     Ok(())
 }
