f3d525e23ceb15a1daf97974592eea2b44273962
Nyctef <nyctef@nyctef.com>
2023-01-01 12:47:53 +0000

    day 14 parsing complete (still learning nom)

--- 2019-rust/src/day14.rs
+++ 2019-rust/src/day14.rs
@@ -4,13 +4,14 @@ use nom::{
     bytes::complete::tag,
     character::complete::{alpha1, digit1},
     combinator::{all_consuming, map},
+    error::ErrorKind,
     multi::separated_list1,
     sequence::separated_pair,
     Finish,
 };
 
 pub fn solve() -> Result<()> {
-    let input = get_input(2019, 1)?;
+    let input = get_input(2019, 14)?;
 
     let result = solve_for(&input)?;
 
@@ -21,20 +22,31 @@ pub fn solve() -> Result<()> {
 fn solve_for(input: &str) -> Result<String> {
     let parsed = parse_input(input)?;
 
+    dbg!(parsed);
+
     todo!()
 }
 
 fn parse_input(input: &str) -> Result<Vec<Recipe>> {
-    let parse_ingredient = map(separated_pair(digit1, tag(" "), alpha1), Ingredient::from);
-    let parse_inputs = separated_list1(tag(", "), parse_ingredient);
+    let parse_ingredient = || {
+        map(
+            separated_pair(digit1::<_, (_, ErrorKind)>, tag(" "), alpha1),
+            Ingredient::from,
+        )
+    };
+    let parse_inputs = separated_list1(tag(", "), parse_ingredient());
     let parse_line = map(
-        separated_pair(parse_inputs, tag(" => "), parse_ingredient),
+        separated_pair(parse_inputs, tag(" => "), parse_ingredient()),
         Recipe::from,
     );
     let parse_lines = separated_list1(tag("\n"), parse_line);
-    Ok(all_consuming(parse_lines)(input).finish()?.1)
+    let result = all_consuming(parse_lines)(input.trim())
+        .finish()
+        .map_err(|e| Report::msg(format!("{:?}", e)));
+    Ok(result?.1)
 }
 
+#[derive(Debug)]
 struct Recipe {
     inputs: Vec<Ingredient>,
     output: Ingredient,
@@ -48,6 +60,7 @@ impl Recipe {
     }
 }
 
+#[derive(Debug)]
 struct Ingredient {
     quantity: u32,
     name: String,
@@ -64,7 +77,12 @@ impl Ingredient {
 #[test]
 fn test_example1() -> Result<()> {
     let input = r###"
-    
+10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL
 "###;
     let result = solve_for(input)?;
 
