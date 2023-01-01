b9d41d8138c80ed523ddd9534b6c8266b51151ab
Nyctef <nyctef@nyctef.com>
2023-01-01 13:52:37 +0000

    slightly better error handling for nom

--- 2019-rust/src/day14.rs
+++ 2019-rust/src/day14.rs
@@ -4,7 +4,7 @@ use nom::{
     bytes::complete::tag,
     character::complete::{alpha1, digit1},
     combinator::{all_consuming, map},
-    error::ErrorKind,
+    error::{convert_error, ErrorKind, VerboseError},
     multi::separated_list1,
     sequence::separated_pair,
     Finish,
@@ -73,21 +73,16 @@ fn div_ceil(a: u32, b: u32) -> u32 {
 }
 
 fn parse_input(input: &str) -> Result<Vec<Recipe>> {
-    let parse_ingredient = || {
-        map(
-            separated_pair(digit1::<_, (_, ErrorKind)>, tag(" "), alpha1),
-            Ingredient::from,
-        )
-    };
+    let parse_ingredient = || map(separated_pair(digit1, tag(" "), alpha1), Ingredient::from);
     let parse_inputs = separated_list1(tag(", "), parse_ingredient());
     let parse_line = map(
         separated_pair(parse_inputs, tag(" => "), parse_ingredient()),
         Recipe::from,
     );
     let parse_lines = separated_list1(tag("\n"), parse_line);
-    let result = all_consuming(parse_lines)(input.trim())
+    let result = all_consuming::<_, _, VerboseError<_>, _>(parse_lines)(input.trim())
         .finish()
-        .map_err(|(r, k)| Report::msg(format!("Failed to match {:?} near {:?}", k, r)));
+        .map_err(|e| Report::msg(convert_error(input, e)));
     Ok(result?.1)
 }
 
@@ -127,7 +122,7 @@ impl Debug for Ingredient {
 fn test_example1() -> Result<()> {
     let input = r###"
 10 ORE => 10 A
-1 ORE => 1 B
+--1 ORE => 1 B
 7 A, 1 B => 1 C
 7 A, 1 C => 1 D
 7 A, 1 D => 1 E
