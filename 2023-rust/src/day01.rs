ca0991f4409783b6a91eabbcee5f511371017e46
Mark Jordan <Mark.Jordan@red-gate.com>
2023-12-01 10:23:54 +0000

    writing some more tests to try and figure out part 2

--- 2023-rust/src/day01.rs
+++ 2023-rust/src/day01.rs
@@ -1,6 +1,5 @@
 use crate::aoc_util::*;
 use color_eyre::eyre::Result;
-use itertools::Itertools;
 
 pub fn solve() -> Result<()> {
     let input = get_input(2023, 1)?;
@@ -12,37 +11,42 @@ pub fn solve() -> Result<()> {
 }
 
 fn solve_for(input: &str) -> Result<String> {
+    let mut sum: u64 = 0;
+    for line in input.lines() {
+        if line.is_empty() {
+            continue;
+        }
+
+        sum += parse_line(line)? as u64;
+    }
+    Ok(sum.to_string())
+}
+
+fn parse_line(line: &str) -> Result<u32> {
     let digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
     let digit_strings = [
         "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
     ];
 
     let digit_re = regex::Regex::new(&format!(
-        r"({})|({})",
+        r"{}|{}",
         digits.join("|"),
         digit_strings.join("|"),
     ))?;
 
-    let mut sum = 0;
-    for line in input.lines() {
-        if line.is_empty() {
-            continue;
-        }
-
-        let mut first_digit = digit_re.find(line).unwrap().as_str();
-        if let Some(i) = digit_strings.iter().position(|&s| s == first_digit) {
-            first_digit = digits[i];
-        }
-
-        let mut last_digit = digit_re.find_iter(line).last().unwrap().as_str();
-        if let Some(i) = digit_strings.iter().position(|&s| s == last_digit) {
-            last_digit = digits[i];
-        }
-
-        let number = format!("{}{}", first_digit, last_digit);
-        sum += number.parse::<u32>()?;
+    let mut first_digit = digit_re.find(line).unwrap().as_str();
+    if let Some(i) = digit_strings.iter().position(|&s| s == first_digit) {
+        first_digit = digits[i];
     }
-    Ok(sum.to_string())
+
+    let mut last_digit = digit_re.find_iter(line).last().unwrap().as_str();
+    if let Some(i) = digit_strings.iter().position(|&s| s == last_digit) {
+        last_digit = digits[i];
+    }
+
+    let number = format!("{}{}", first_digit, last_digit);
+    println!("{}\t -> \t{}", line, number);
+    Ok(number.parse::<u32>()?)
 }
 
 #[test]
@@ -74,4 +78,16 @@ zoneight234
 
     assert_eq!("281", result);
     Ok(())
+}
+
+#[test]
+fn parse_lines() {
+    assert_eq!(31, parse_line("3fiveone").unwrap());
+    assert_eq!(
+        31,
+        parse_line("zstxvfdthreeseven7mdfpgzgfourdfshplvqflfprt1").unwrap()
+    );
+    assert_eq!(77, parse_line("7").unwrap());
+    assert_eq!(77, parse_line("seven").unwrap());
+    assert_eq!(22, parse_line("2xq").unwrap());
 }
