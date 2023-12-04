1dc8e5b2e49f01bb0aff51232db49964d5672db6
Nyctef <nyctef@nyctef.com>
2023-12-04 07:16:29 +0000

    day 4 part 2

--- 2023-rust/src/day04.rs
+++ 2023-rust/src/day04.rs
@@ -1,5 +1,6 @@
 use crate::utils::*;
 use color_eyre::eyre::Result;
+use itertools::Itertools;
 use std::collections::HashSet;
 
 pub fn solve() -> Result<()> {
@@ -22,21 +23,52 @@ fn all_numbers(input: &str) -> Vec<u32> {
 fn solve_for(input: &str) -> Result<String> {
     let mut point_total = 0;
 
-    for line in input.trim().lines() {
-        let (_, line) = line.split_once(':').unwrap();
+    let mut card_list: Vec<u32> = Vec::new();
+
+    let lines = input.trim().lines().collect_vec();
+    for line in &lines {
+        let (num, line) = line.split_once(':').unwrap();
+        // let num = all_numbers(num).iter().exactly_one();
+
         let (winners, have) = line.split_once('|').unwrap();
         let (winners, have) = (all_numbers(winners), all_numbers(have));
         let winners: HashSet<_> = HashSet::from_iter(winners);
         let have: HashSet<_> = HashSet::from_iter(have);
         let in_common: HashSet<_> = winners.intersection(&have).collect();
         let num_in_common: usize = in_common.len();
+
         if num_in_common > 0 {
             // dbg!(in_common, num_in_common, 2u32.pow(num_in_common as u32 - 1));
             point_total += 2u32.pow(num_in_common as u32 - 1);
         }
+
+        card_list.push(num_in_common as u32);
     }
 
-    Ok(format!("Part 1: {point_total} | Part 2:"))
+    let mut card_counts: Vec<u32> = Vec::from_iter(std::iter::repeat(1).take(lines.len()));
+
+    for (card_index, &num_in_common_for_card) in card_list.iter().enumerate() {
+        println!("considering {card_index} with {num_in_common_for_card} matches");
+        if num_in_common_for_card > 0 {
+            // println!(
+            //     "updating card counts for {} to {} with {}",
+            //     (card_index + 1),
+            //     (card_index + (num_in_common_for_card as usize)),
+            //     card_counts[card_index]
+            // );
+            for future_card in
+                (card_index + 1)..(card_index + (num_in_common_for_card as usize) + 1)
+            {
+                card_counts[future_card] += card_counts[card_index];
+            }
+            // dbg!(&card_counts);
+        }
+    }
+    // dbg!(&card_list, &card_counts);
+
+    let card_total: u32 = card_counts.iter().sum();
+
+    Ok(format!("Part 1: {point_total} | Part 2: {card_total}"))
 }
 
 #[test]
@@ -51,6 +83,6 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 "###;
     let result = solve_for(input)?;
 
-    assert_eq!("Part 1: 13 | Part 2:", result);
+    assert_eq!("Part 1: 13 | Part 2: 30", result);
     Ok(())
 }
