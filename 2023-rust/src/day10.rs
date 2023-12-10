b70ff438d0acac3ac75a562c5dd4e36ac3cbf9bd
Nyctef <nyctef@nyctef.com>
2023-12-10 07:28:37 +0000

    tests for part 2

--- 2023-rust/src/day10.rs
+++ 2023-rust/src/day10.rs
@@ -62,7 +62,7 @@ fn solve_for(input: &str) -> Result<String> {
         }
     }
 
-    println!("{} {}", seen.len(), seen.len() / 2);
+    // println!("{} {}", seen.len(), seen.len() / 2);
 
     let part1 = seen.len() / 2;
     let part2 = "";
@@ -109,7 +109,7 @@ L|-JF
 "###;
     let result = solve_for(input)?;
 
-    assert_eq!("Part 1: 4 | Part 2: ", result);
+    assert_eq!("Part 1: 4 | Part 2: 1", result);
     Ok(())
 }
 
@@ -124,6 +124,65 @@ LJ...
 "###;
     let result = solve_for(input)?;
 
-    assert_eq!("Part 1: 8 | Part 2: ", result);
+    assert_eq!("Part 1: 8 | Part 2: 1", result);
+    Ok(())
+}
+
+#[test]
+fn test_example3() -> Result<()> {
+    let input = r###"
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
+"###;
+    let result = solve_for(input)?;
+
+    assert_eq!("Part 1: 23 | Part 2: 4", result);
+    Ok(())
+}
+
+#[test]
+fn test_example4() -> Result<()> {
+    let input = r###"
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...
+"###;
+    let result = solve_for(input)?;
+
+    assert_eq!("Part 1: 70 | Part 2: 8", result);
+    Ok(())
+}
+
+#[test]
+fn test_example5() -> Result<()> {
+    let input = r###"
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L
+"###;
+    let result = solve_for(input)?;
+
+    assert_eq!("Part 1: 80 | Part 2: 10", result);
     Ok(())
 }
