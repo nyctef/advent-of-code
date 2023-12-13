1ce1dfa61b9067aa37ae68d89539326be62b3b62
Nyctef <nyctef@nyctef.com>
2023-12-13 06:58:27 +0000

    some day 13 cleanup

--- 2023-rust/src/day13.rs
+++ 2023-rust/src/day13.rs
@@ -14,33 +14,26 @@ pub fn solve() -> Result<()> {
 fn solve_for(input: &str) -> Result<String> {
     let patterns = input.split("\n\n").map(|x| x.trim());
 
-    let mut total = 0;
+    let mut part1 = 0;
+    let mut part2 = 0;
     for pattern in patterns {
-
-        println!("----");
-        println!("{}", pattern);
-        println!("----");
-        total += solve_for_pattern(pattern);
+        part1 += solve_for_pattern(pattern, 0);
+        part2 += solve_for_pattern(pattern, 1);
     }
 
-    let part1 = total;
-    let part2 = "";
     Ok(format!("Part 1: {part1} | Part 2: {part2}"))
 }
 
-fn solve_for_pattern(input: &str) -> u32 {
+fn solve_for_pattern(input: &str, target_score: u32) -> u32 {
     let grid = CharGrid::from_string(input);
     for r in 0..grid.height() - 1 {
-        let row_score = score_row_mirror(&grid, r);
-        println!("testing row {r}: got score {row_score}");
-        if row_score == 1 {
+        if score_row_mirror(&grid, r) == target_score {
             return (r as u32 + 1) * 100;
         }
     }
     for c in 0..grid.width() - 1 {
-        // println!("testing col {c}");
-        if score_col_mirror(&grid, c) == 1 {
-            return (c as u32 + 1);
+        if score_col_mirror(&grid, c) == target_score {
+            return c as u32 + 1;
         }
     }
     panic!("no mirrors found");
@@ -52,13 +45,15 @@ fn score_row_mirror(grid: &CharGrid, start: usize) -> u32 {
     let mut r = start + 1;
     let all_lines = grid.lines().collect_vec();
     let mut score = 0;
-    while l >= 0 && r < grid.height() {
+    while r < grid.height() {
         for (lc, rc) in all_lines[l].chars().zip(all_lines[r].chars()) {
-            if lc != rc { score += 1 }
+            if lc != rc {
+                // an imperfection! let's see if we only get one...
+                score += 1
+            }
         }
 
-
-        // l -= 1;
+        // since l is usize it can't actually go below 0, so we have to check manually here
         if l == 0 {
             break;
         }
@@ -73,12 +68,15 @@ fn score_col_mirror(grid: &CharGrid, start: usize) -> u32 {
     let mut r = start + 1;
     let all_cols = grid.cols().collect_vec();
     let mut score = 0;
-    while l >= 0 && r < grid.width() {
+    while r < grid.width() {
         for (lc, rc) in all_cols[l].iter().zip(all_cols[r].iter()) {
-            if lc != rc { score += 1 }
+            if lc != rc {
+                // an imperfection! let's see if we only get one...
+                score += 1
+            }
         }
 
-        // l -= 1;
+        // since l is usize it can't actually go below 0, so we have to check manually here
         if l == 0 {
             break;
         }
@@ -99,7 +97,7 @@ fn test_example1() -> Result<()> {
 ..##..##.
 #.#.##.#.
 "###;
-    let result = solve_for_pattern(input);
+    let result = solve_for_pattern(input, 0);
 
     assert_eq!(5, result);
     Ok(())
@@ -116,7 +114,7 @@ fn test_example2() -> Result<()> {
 ..##..###
 #....#..#
 "###;
-    let result = solve_for_pattern(input);
+    let result = solve_for_pattern(input, 0);
 
     assert_eq!(400, result);
     Ok(())
