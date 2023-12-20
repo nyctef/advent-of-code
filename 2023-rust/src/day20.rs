08bdbba91b7ebe7531e5be8ab3f46f3578b6c0d6
Nyctef <nyctef@nyctef.com>
2023-12-20 12:02:28 +0000

    turns out we don't need to clone strings nearly as much
    so long as we tell rust explicitly that for any container that contains
    `&str`s, those strs have the same lifetime as the input str

--- 2023-rust/src/day20.rs
+++ 2023-rust/src/day20.rs
@@ -12,40 +12,37 @@ pub fn solve() -> Result<()> {
     Ok(())
 }
 
-fn solve_for(input: &str) -> Result<String> {
+fn solve_for<'input>(input: &'input str) -> Result<String> {
     let lines = input.trim().lines();
     let nodes = lines.map(|l| l.split_once(" -> ").unwrap());
-    let mut modules: HashMap<_, _> = nodes
+    let mut modules: HashMap<&'input str, _> = nodes
         .map(|(n, t)| {
-            let targets = t.split(',').map(|x| x.trim().to_owned()).collect_vec();
+            let targets = t.split(',').map(|x| x.trim()).collect_vec();
             if n == "broadcaster" {
-                (n.to_owned(), Module::Broadcaster(targets))
+                (n, Module::Broadcaster(targets))
             } else if let Some(name) = n.strip_prefix('%') {
-                (name.to_owned(), Module::Flipflop(false, targets))
+                (name, Module::Flipflop(false, targets))
             } else if let Some(name) = n.strip_prefix('&') {
-                (
-                    name.to_owned(),
-                    Module::Conjunction(HashMap::new(), targets),
-                )
+                (name, Module::Conjunction(HashMap::new(), targets))
             } else {
                 panic!("can't parse {} {}", n, t);
             }
         })
         .collect();
 
-    modules.insert("output".to_owned(), Module::Output);
+    modules.insert(&"output", Module::Output);
 
-    let mut target_links = vec![];
+    let mut target_links: Vec<(&'input str, &'input str)> = vec![];
     for (source_name, source_module) in modules.iter() {
         for target_name in get_targets(source_module) {
-            target_links.push((source_name.clone(), target_name.clone()));
+            target_links.push((source_name, target_name));
         }
     }
 
     for (source_name, target_name) in target_links {
-        if let Entry::Occupied(mut target_module) = modules.entry(target_name.clone()) {
+        if let Entry::Occupied(mut target_module) = modules.entry(target_name) {
             if let Module::Conjunction(ref mut inputs, _) = target_module.get_mut() {
-                inputs.insert(source_name.clone(), false);
+                inputs.insert(&source_name, false);
             }
         } else {
             // turns out we might send signals to an undefined module (this is intended)
@@ -69,13 +66,13 @@ fn solve_for(input: &str) -> Result<String> {
         // pushing the button sends a low signal to broadcaster
         track_signal(false);
         button_presses += 1;
-        let mut queue = VecDeque::new();
-        let broadcaster = &modules["broadcaster"];
+        let mut queue: VecDeque<(&'input str, &'input str, bool)> = VecDeque::new();
+        let broadcaster = &modules[&"broadcaster"];
         let Module::Broadcaster(broadcast_targets) = broadcaster else {
             panic!("broadcaster not a broadcaster")
         };
         for bt in broadcast_targets {
-            queue.push_back(("broadcaster".to_owned(), bt.clone(), false));
+            queue.push_back(("broadcaster", bt, false));
         }
 
         while let Some((source, dest, sig_is_high)) = queue.pop_front() {
@@ -87,10 +84,11 @@ fn solve_for(input: &str) -> Result<String> {
                 }
 
                 if !sig_is_high {
+                    // this takes a while to hit
                     break 'outer;
                 }
             }
-            let target_module = modules.get_mut(&dest.clone());
+            let target_module = modules.get_mut(dest);
 
             match target_module {
                 Some(Module::Broadcaster(_)) => panic!("???"),
@@ -100,12 +98,12 @@ fn solve_for(input: &str) -> Result<String> {
                     } else {
                         *ff_is_on = !*ff_is_on;
                         for t in ff_targets {
-                            queue.push_back((dest.clone(), t.clone(), *ff_is_on));
+                            queue.push_back((dest, t, *ff_is_on));
                         }
                     }
                 }
                 Some(Module::Conjunction(ref mut inputs, c_targets)) => {
-                    inputs.insert(source.clone(), sig_is_high);
+                    inputs.insert(&source, sig_is_high);
 
                     if dest == "cn" && inputs.values().any(|i| *i) {
                         println!("cn: bp {} inputs {:?}", button_presses, inputs);
@@ -113,7 +111,7 @@ fn solve_for(input: &str) -> Result<String> {
 
                     let output_is_high = !inputs.values().all(|i| *i);
                     for t in c_targets {
-                        queue.push_back((dest.clone(), t.clone(), output_is_high));
+                        queue.push_back((dest, t, output_is_high));
                     }
                 }
                 _ => {}
@@ -127,7 +125,7 @@ fn solve_for(input: &str) -> Result<String> {
     Ok(format!("Part 1: {part1} | Part 2: {part2}"))
 }
 
-fn get_targets(module: &Module) -> Vec<String> {
+fn get_targets<'input>(module: &Module<'input>) -> Vec<&'input str> {
     match module {
         Module::Output => vec![],
         Module::Broadcaster(t) => t.clone(),
@@ -137,14 +135,14 @@ fn get_targets(module: &Module) -> Vec<String> {
 }
 
 #[derive(Debug, Eq, PartialEq)]
-enum Module {
+enum Module<'input> {
     Output,
     // targets
-    Broadcaster(Vec<String>),
+    Broadcaster(Vec<&'input str>),
     // is_on, targets
-    Flipflop(bool, Vec<String>),
+    Flipflop(bool, Vec<&'input str>),
     // for each input: are we remembering high?, targets
-    Conjunction(HashMap<String, bool>, Vec<String>),
+    Conjunction(HashMap<&'input str, bool>, Vec<&'input str>),
 }
 
 #[test]
