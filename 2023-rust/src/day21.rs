e57a9869ffaa97c3aa055da23f06c7f4dd46acd4
Nyctef <nyctef@nyctef.com>
2023-12-21 08:43:11 +0000

    expanded coordinates to pass for 10 steps

--- 2023-rust/src/day21.rs
+++ 2023-rust/src/day21.rs
@@ -1,13 +1,13 @@
-use std::collections::HashSet;
-
 use crate::utils::*;
 use color_eyre::eyre::Result;
+use derive_more::Constructor;
 use itertools::Itertools;
+use std::{collections::HashSet, ops::Add};
 
 pub fn solve() -> Result<()> {
     let input = get_input(2023, 21)?;
 
-    let result = solve_for(&input,26501365 )?;
+    let result = solve_for(&input, 26501365)?;
 
     println!("{}", result);
     Ok(())
@@ -16,12 +16,13 @@ pub fn solve() -> Result<()> {
 fn solve_for(input: &str, step_count: usize) -> Result<String> {
     let grid = CharGrid::from_string(input);
 
-    let mut start = grid
+    let start = grid
         .enumerate_chars_rc()
         .filter(|(_p, c)| c == &'S')
         .exactly_one()
         .map(|(p, _c)| p)
         .unwrap();
+    let start = WrappingIndexRC::new(start.row as isize, start.col as isize);
     let mut starting_set = HashSet::new();
     starting_set.insert(start);
     let mut next_step = HashSet::new();
@@ -29,15 +30,11 @@ fn solve_for(input: &str, step_count: usize) -> Result<String> {
         for p in &starting_set {
             for dir in RCDirection::four() {
                 let p2 = *p + dir;
-                if p2 == *p || !grid.is_in_bounds(p2) {
-                    continue;
-                }
-                if grid[p2] == '#' {
+                if grid[p2.wrap(grid.width(), grid.height())] == '#' {
                     continue;
                 }
                 next_step.insert(p2);
             }
-
         }
 
         starting_set.clear();
@@ -46,6 +43,32 @@ fn solve_for(input: &str, step_count: usize) -> Result<String> {
     }
 
     Ok(format!("total: {}", starting_set.len()))
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Constructor, Hash)]
+pub struct WrappingIndexRC {
+    pub row: isize,
+    pub col: isize,
+}
+
+impl WrappingIndexRC {
+    fn wrap(&self, width: usize, height: usize) -> CharGridIndexRC {
+        CharGridIndexRC::new(
+            self.row.rem_euclid(width as isize) as usize,
+            self.col.rem_euclid(height as isize) as usize,
+        )
+    }
+}
+
+impl Add<RCDirection> for WrappingIndexRC {
+    type Output = WrappingIndexRC;
+
+    fn add(self, rhs: RCDirection) -> Self::Output {
+        Self::Output {
+            row: self.row + rhs.rowdiff,
+            col: self.col + rhs.coldiff,
+        }
+    }
 }
 
 #[test]
