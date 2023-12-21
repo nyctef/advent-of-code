ed03e7af1440052e4af3e13a9b7cb23c6c92d814
Nyctef <nyctef@nyctef.com>
2023-12-21 10:03:06 +0000

    reworking grid coords so we're not constantly recalculating grids

--- 2023-rust/src/day21.rs
+++ 2023-rust/src/day21.rs
@@ -6,6 +6,7 @@ use num::traits::Euclid;
 use std::{
     collections::{HashMap, HashSet},
     ops::Add,
+    task::Wake,
 };
 
 pub fn solve() -> Result<()> {
@@ -26,13 +27,12 @@ fn solve_for(input: &str, step_count: usize) -> Result<String> {
         .exactly_one()
         .map(|(p, _c)| p)
         .unwrap();
-    let start = WrappingIndexRC::new(start.row as isize, start.col as isize);
     let mut starting_set = HashSet::new();
-    starting_set.insert(start);
+    starting_set.insert(GGIndexRC::new(0, 0, start.row, start.col));
     let mut next_step = HashSet::new();
     let mut final_total: u64 = 0;
     let parity = step_count % 2;
-    
+
     // TODO: something like: if we see a grid go into a cycle, and the current step count is
     // even or odd according to the final step count, then remove all the points from that grid
     // and add that grid's current count of points to the final total
@@ -40,26 +40,25 @@ fn solve_for(input: &str, step_count: usize) -> Result<String> {
     let mut central_grid_seen_states = HashMap::new();
 
     for step in 0..step_count {
-        let mut points_by_grid: HashMap<(isize, isize), Vec<CharGridIndexRC>> = HashMap::new();
+        let mut points_by_grid: HashMap<(isize, isize), Vec<GGIndexRC>> = HashMap::new();
 
         for (key, group) in &starting_set
             .iter()
-            .map(|p| {
-                (
-                    (
-                        div_floor(p.row, grid.height() as isize),
-                        div_floor(p.col, grid.width() as isize),
-                    ),
-                    p.wrap(grid.width(), grid.height()),
-                )
-            })
-            .group_by(|gp| gp.0)
+            // .map(|p| {
+            //     (
+            //         (
+            //             div_floor(p.row, grid.height() as isize),
+            //             div_floor(p.col, grid.width() as isize),
+            //         ),
+            //         p.wrap(grid.width(), grid.height()),
+            //     )
+            // })
+            .group_by(|gp| (gp.x, gp.y))
         {
             let collection = points_by_grid.entry(key).or_default();
-            collection.extend(group.map(|gp| gp.1));
+            collection.extend(group);
             // todo: this repeated sorting is probably slow
             collection.sort_by_key(|p| (p.row, p.col));
-
         }
         let points_inside_original_grid = &points_by_grid[&(0, 0)];
         // if !(points_inside_original_grid == by_division) {
@@ -83,9 +82,8 @@ fn solve_for(input: &str, step_count: usize) -> Result<String> {
         }
 
         for p in &starting_set {
-            for dir in RCDirection::four() {
-                let p2 = *p + dir;
-                if grid[p2.wrap(grid.width(), grid.height())] == '#' {
+            for p2 in [p.up(&grid), p.right(&grid), p.down(&grid), p.left(&grid)] {
+                if grid.index_rc(p2.row, p2.col) == '#' {
                     continue;
                 }
                 next_step.insert(p2);
@@ -98,6 +96,57 @@ fn solve_for(input: &str, step_count: usize) -> Result<String> {
     }
 
     Ok(format!("total: {}", starting_set.len()))
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Constructor, Hash)]
+pub struct GGIndexRC {
+    // which grid we're on
+    pub x: isize,
+    // we'll make positive y downwards just so it's the same direction as rows
+    pub y: isize,
+    // row/col within that grid
+    pub row: usize,
+    pub col: usize,
+}
+
+impl GGIndexRC {
+    pub fn up(&self, grid: &CharGrid) -> Self {
+        if self.row == 0 {
+            Self::new(self.x, self.y - 1, grid.height() - 1, self.col)
+        } else {
+            Self::new(self.x, self.y, self.row - 1, self.col)
+        }
+    }
+    pub fn down(&self, grid: &CharGrid) -> Self {
+        if self.row == grid.height() - 1 {
+            Self::new(self.x, self.y + 1, 0, self.col)
+        } else {
+            Self::new(self.x, self.y, self.row + 1, self.col)
+        }
+    }
+    pub fn right(&self, grid: &CharGrid) -> Self {
+        if self.col == grid.width() - 1 {
+            Self::new(self.x + 1, self.y, self.row, 0)
+        } else {
+            Self::new(self.x, self.y, self.row, self.col + 1)
+        }
+    }
+    pub fn left(&self, grid: &CharGrid) -> Self {
+        if self.col == 0 {
+            Self::new(self.x - 1, self.y, self.row, grid.width() - 1)
+        } else {
+            Self::new(self.x, self.y, self.row, self.col - 1)
+        }
+    }
+}
+
+impl std::fmt::Debug for GGIndexRC {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "GG(gx{} gy{} r{} c{})",
+            self.x, self.y, self.row, self.col
+        ))
+    }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Constructor, Hash)]
