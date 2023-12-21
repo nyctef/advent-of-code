72b406781b1cc08a6220db71ad5ad8ac7f4d33dd
Nyctef <nyctef@nyctef.com>
2023-12-21 08:32:51 +0000

    put this first example in a test

--- 2023-rust/src/day21.rs
+++ 2023-rust/src/day21.rs
@@ -7,13 +7,13 @@ use itertools::Itertools;
 pub fn solve() -> Result<()> {
     let input = get_input(2023, 21)?;
 
-    let result = solve_for(&input)?;
+    let result = solve_for(&input,26501365 )?;
 
     println!("{}", result);
     Ok(())
 }
 
-fn solve_for(input: &str) -> Result<String> {
+fn solve_for(input: &str, step_count: usize) -> Result<String> {
     let grid = CharGrid::from_string(input);
 
     let mut start = grid
@@ -25,7 +25,7 @@ fn solve_for(input: &str) -> Result<String> {
     let mut starting_set = HashSet::new();
     starting_set.insert(start);
     let mut next_step = HashSet::new();
-    for _ in 0..64 {
+    for _ in 0..step_count {
         for p in &starting_set {
             for dir in RCDirection::four() {
                 let p2 = *p + dir;
@@ -45,18 +45,26 @@ fn solve_for(input: &str) -> Result<String> {
         next_step.clear();
     }
 
-    let part1 = starting_set.len();
-    let part2 = "";
-    Ok(format!("Part 1: {part1} | Part 2: {part2}"))
+    Ok(format!("total: {}", starting_set.len()))
 }
 
 #[test]
 fn test_example1() -> Result<()> {
     let input = r###"
-    
+...........
+.....###.#.
+.###.##..#.
+..#.#...#..
+....#.#....
+.##..S####.
+.##..#...#.
+.......##..
+.##.#.####.
+.##..##.##.
+...........
 "###;
-    let result = solve_for(input)?;
+    let result = solve_for(input, 6)?;
 
-    assert_eq!("Part 1: | Part 2: ", result);
+    assert_eq!("total: 16", result);
     Ok(())
 }
