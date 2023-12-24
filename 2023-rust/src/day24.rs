4ef67a16e8c05f3fd4ba870bcb3f91b7115df558
Nyctef <nyctef@nyctef.com>
2023-12-24 07:15:20 +0000

    day 24 part 1

--- 2023-rust/src/day24.rs
+++ 2023-rust/src/day24.rs
@@ -104,20 +104,38 @@ fn solve_for(input: &str) -> Result<String> {
             */
 
             let t_b = (hb.y0 - ha.y0 - ((hb.x0 - ha.x0) * (ha.dy / ha.dx))) / (((hb.dx * ha.dy / ha.dx) - hb.dy));
-            let t_a = (hb.x0 + t_b * hb.dx) / ha.dx;
+            let t_a = (hb.x0 + t_b * hb.dx - ha.x0) / ha.dx;
 
             println!("t_b {:.3} t_a {:.3}", t_b, t_a);
-            if t_a.is_infinite() || t_b.is_infinite() {
-                println!("nonintersecting");
-                continue;
-            }
 
             let x_a = ha.x0 + (ha.dx * t_a);
             let y_a = ha.y0 + (ha.dy * t_a);
             let x_b = hb.x0 + (hb.dx * t_b);
             let y_b = hb.y0 + (hb.dy * t_b);
 
-            println!("a: {:.3} {:.3} b: {:.3} {:.3}", x_a, y_a, x_b, y_b);
+            // println!("a: {} {} b: {} {}", x_a, y_a, x_b, y_b);
+
+            // can't assert this easily because floats
+            // assert!(x_a == x_b && y_a == y_b);
+            if t_a.is_infinite() || t_b.is_infinite() {
+                println!("nonintersecting");
+                continue;
+            }
+            if t_a < 0.0 || t_b < 0.0 {
+                println!("past");
+                continue;
+            }
+            let test_area_min = 200000000000000.;
+            let test_area_max = 400000000000000.;
+
+            if x_a < test_area_min || y_a < test_area_min || x_a > test_area_max || y_a > test_area_max {
+                println!("out of bounds");
+                continue;
+            }
+
+
+            println!("intersection at {} {}", x_a, y_a);
+            collisions += 1;
         }
     }
 
