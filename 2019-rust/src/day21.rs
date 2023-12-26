6a9e6e1b4dee14612ad7352b9ae6f5efa05a439a
Nyctef <nyctef@nyctef.com>
2023-12-26 16:39:56 +0000

    split up instructions

--- 2019-rust/src/day21.rs
+++ 2019-rust/src/day21.rs
@@ -14,16 +14,25 @@ pub fn solve() -> Result<()> {
 fn solve_for(input: &str) -> Result<String> {
     let mut intcode = IntCode::from_str(input)?;
 
-    for c in "NOT A J
-NOT B T
-OR T J
-NOT C T
-OR T J
-AND D J
-WALK
-".chars() {
+    let instructions = [
+        // jump if gap one steps ahead
+        "NOT A J",
+        // jump if gap two steps ahead
+        "NOT B T\nOR T J",
+        // jump if gap three steps ahead
+        "NOT C T\nOR T J",
+        // only jump if will land on ground
+        "AND D J",
+        // only jump if we can 1. jump immediately or 2. go forward
+        // after landing
+        "AND H T\nOR E T\nAND T J",
+        "RUN"
+
+    ];
+    for c in instructions.join("\n").chars() {
         intcode.queue_input(c as i64);
     }
+    intcode.queue_input('\n' as i64);
 
     intcode.run()?;
 
