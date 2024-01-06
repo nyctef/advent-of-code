999d105d95be6ca16cf28b5255f30233d4ba7c76
Nyctef <nyctef@nyctef.com>
2024-01-06 12:48:59 +0000

    parse rules, making a distinction between terminals and nonterminals

--- 2015-rust/src/bin/day19.rs
+++ 2015-rust/src/bin/day19.rs
@@ -1,6 +1,8 @@
 use aoc_2015_rust::util::*;
 use color_eyre::eyre::Result;
+use derive_more::Constructor;
 use itertools::Itertools;
+use std::fmt::Display;
 
 pub fn main() -> Result<()> {
     color_eyre::install()?;
@@ -15,7 +17,45 @@ pub fn main() -> Result<()> {
 
 fn solve_for(input: &str) -> Result<String> {
     let (replacements, target) = input.trim().split_once("\n\n").unwrap();
-    let _replacements = parse_replacements(replacements);
+    let replacements = parse_replacements(replacements);
+    let nonterminals = replacements
+        .iter()
+        .map(|(r, _)| Term::Nonterminal(r))
+        .collect_vec();
+    // println!("nonterminals {nonterminals:?}");
+    let mut rules = replacements
+        .iter()
+        .map(|(r, e)| {
+            Rule::new(
+                Term::Nonterminal(r),
+                e.iter()
+                    .map(
+                        |t| match nonterminals.iter().find(|nt| nt.get_value() == *t) {
+                            // if we find a matching element in `nonterminals`, we could just
+                            // return that, but we need to return something owned here so that
+                            // both match arms return the same type of value
+                            Some(_) => Term::Nonterminal(t),
+                            None => Term::Terminal(t),
+                        },
+                    )
+                    .collect_vec(),
+            )
+        })
+        .collect_vec();
+
+    // every nonterminal here can also end up as an equivalent terminal symbol,
+    // so we need to add those rules
+    rules.extend(
+        rules
+            .iter()
+            .flat_map(|r| &r.expansion)
+            .filter(|e| matches!(e, Term::Nonterminal(_)))
+            .unique()
+            .map(|nt| Rule::new(nt.clone(), vec![Term::Terminal(nt.get_value())]))
+            .collect_vec(),
+    );
+
+    dbg!(rules);
     let _target = tokenize(target);
 
     // println!("r {:?} t {:?}", replacements, target);
@@ -34,6 +74,48 @@ fn parse_replacements(replacements: &str) -> Vec<(&str, Vec<&str>)> {
         .map(|l| l.split_once(" => ").unwrap())
         .map(|(s, d)| (s, tokenize(d)))
         .collect_vec()
+}
+
+#[derive(Eq, PartialEq, Hash, Clone)]
+enum Term<'i> {
+    Terminal(&'i str),
+    Nonterminal(&'i str),
+}
+
+impl Display for Term<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Term::Terminal(t) => f.write_fmt(format_args!("'{}'", t)),
+            Term::Nonterminal(nt) => f.write_fmt(format_args!("<{}>", nt)),
+        }
+    }
+}
+
+impl std::fmt::Debug for Term<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{}", self.to_string()))
+    }
+}
+
+impl<'i> Term<'i> {
+    fn get_value(&self) -> &'i str {
+        match self {
+            Term::Terminal(t) => t,
+            Term::Nonterminal(nt) => nt,
+        }
+    }
+}
+
+#[derive(Eq, PartialEq, Constructor)]
+struct Rule<'i> {
+    matches: Term<'i>,
+    expansion: Vec<Term<'i>>,
+}
+
+impl std::fmt::Debug for Rule<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{:?} => {:?}", self.matches, self.expansion))
+    }
 }
 
 fn tokenize(input: &str) -> Vec<&str> {
