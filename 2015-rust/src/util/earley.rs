f69073b825bc2ac5acb280ee3ed6dc52fdfff536
Nyctef <nyctef@nyctef.com>
2024-01-06 13:24:41 +0000

    earley state struct

--- 2015-rust/src/util/earley.rs
+++ 2015-rust/src/util/earley.rs
@@ -1,6 +1,10 @@
 use derive_more::Constructor;
 use std::fmt::Display;
 
+// TODO: make this generic in the token type rather than assuming `&'input str` ?
+
+// largely following https://rahul.gopinath.org/post/2021/02/06/earley-parsing/
+
 #[derive(Eq, PartialEq, Hash, Clone)]
 pub enum Term<'i> {
     Terminal(&'i str),
@@ -40,5 +44,63 @@ pub struct Rule<'i> {
 impl std::fmt::Debug for Rule<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("{:?} => {:?}", self.matches, self.expansion))
+    }
+}
+
+#[derive(Eq, PartialEq, Constructor)]
+pub struct State<'r, 'i> {
+    rule: &'r Rule<'i>,
+    dot: usize,
+    start_col: usize,
+    end_col: Option<usize>,
+}
+
+impl std::fmt::Debug for State<'_, '_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!(
+            "{:?} => {:?} | {:?} ({}, {:?})",
+            self.rule.matches,
+            &self.rule.expansion[..self.dot],
+            &self.rule.expansion[self.dot..],
+            self.start_col,
+            self.end_col
+        ))
+    }
+}
+
+impl<'i, 'r> State<'i, 'r> {
+    fn advance(&mut self) {
+        assert!(self.dot < self.rule.expansion.len());
+        self.dot += 1
+    }
+
+    fn finished(&self) -> bool {
+        self.dot >= self.rule.expansion.len()
+    }
+
+    fn at_dot(&self) -> Option<&'r Term<'i>> {
+        if self.dot < self.rule.expansion.len() {
+            Some(&self.rule.expansion[self.dot])
+        } else {
+            None
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::Term::*;
+    use super::*;
+
+    #[test]
+    fn basic_state_tests() {
+        let rule = Rule::new(Nonterminal("A"), vec![Terminal("a"), Terminal("b")]);
+        let mut state = State::new(&rule, 0, 0, None);
+
+        assert_eq!(format!("{:?}", state), "<A> => [] | ['a', 'b'] (0, None)");
+
+        state.advance();
+
+        assert_eq!(format!("{:?}", state), "<A> => ['a'] | ['b'] (0, None)");
     }
 }
