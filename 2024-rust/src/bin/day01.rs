1e5f6d576cd9cbf3cf1ec4a80444ca055ac7e073
Nyctef <nyctef@nyctef.com>
2024-12-01 08:18:27 +0000

    day 1 part 2

--- 2024-rust/src/bin/day01.rs
+++ 2024-rust/src/bin/day01.rs
@@ -1,6 +1,7 @@
 use aoc_2024_rust::util::*;
 use color_eyre::eyre::Result;
 use itertools::Itertools;
+use rustc_hash::FxHashMap;
 
 pub fn main() -> Result<()> {
     color_eyre::install()?;
@@ -23,7 +24,11 @@ fn solve_for(input: &str) -> Result<String> {
 
     let part1 = list_1.iter().zip_eq(list_2.iter()).map(|(&i1, &i2)| (i2 as i32 - i1 as i32).abs()).sum::<i32>();
 
-    let part2 = "";
+    let list_2_occurrences = list_2.iter().fold(FxHashMap::<u32, u32>::default(), |mut b, &n| {  *(b.entry(n).or_default()) += 1; b } );
+
+    let list_1_similarities = list_1.iter().map(|n| n * list_2_occurrences.get(n).unwrap_or(&0)).sum::<u32>();
+
+    let part2 = list_1_similarities;
     Ok(format!("Part 1: {part1} | Part 2: {part2}"))
 }
 
@@ -39,6 +44,6 @@ fn test_example1() -> Result<()> {
 "###;
     let result = solve_for(input)?;
 
-    assert_eq!("Part 1: 11 | Part 2: ", result);
+    assert_eq!("Part 1: 11 | Part 2: 31", result);
     Ok(())
 }
