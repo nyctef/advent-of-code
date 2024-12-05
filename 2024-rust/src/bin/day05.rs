52db5411008e2c7b67d4f443015a040a5b1e3596
Mark Jordan <Mark.Jordan@red-gate.com>
2024-12-05 12:24:36 +0000

    clippy/fmt

--- 2024-rust/src/bin/day05.rs
+++ 2024-rust/src/bin/day05.rs
@@ -1,7 +1,4 @@
-use std::{
-    cmp::Ordering,
-    collections::{HashMap, HashSet},
-};
+use std::{cmp::Ordering, collections::HashMap};
 
 use aoc_2024_rust::util::*;
 use color_eyre::eyre::Result;
