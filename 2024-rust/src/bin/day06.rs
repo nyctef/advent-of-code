ff980d1c4183e39a88f2f28535bf109fb14aaa3a
Nyctef <nyctef@nyctef.com>
2024-12-06 06:24:00 +0000

    day 6 part 2

--- 2024-rust/src/bin/day06.rs
+++ 2024-rust/src/bin/day06.rs
@@ -19,14 +19,15 @@ fn solve_for(input: &str) -> Result<(u64, u64)> {
     let grid = CharGrid::from_string(input);
     // TODO: why doesn't this compile?
     // let (guard_pos, _) = grid.enumerate_chars_rc().filter(|(pos, c): &(CharGridIndexRC, char)| *c == '^').exactly_one()?;
-    let mut guard_pos = CharGridIndexRC::zero();
+    let mut guard_original_pos = CharGridIndexRC::zero();
     for (pos, char) in grid.enumerate_chars_rc() {
         if char == '^' {
-            guard_pos = pos;
+            guard_original_pos = pos;
             break;
         }
     }
 
+    let mut guard_pos = guard_original_pos;
     let mut guard_direction = RCDirection::up();
     let mut guard_visited_positions = HashSet::new();
 
@@ -44,8 +45,52 @@ fn solve_for(input: &str) -> Result<(u64, u64)> {
         guard_pos = next_pos;
     }
 
-    let part2 = 0;
-    Ok((guard_visited_positions.len().try_into().unwrap(), part2))
+    let mut obstacle_placements = HashSet::new();
+    for possible_obstacle in grid.iter_positions_rc() {
+        if does_guard_loop(&grid, possible_obstacle) && possible_obstacle != guard_original_pos {
+            obstacle_placements.insert(possible_obstacle);
+        }
+    }
+
+    Ok((
+        guard_visited_positions.len().try_into().unwrap(),
+        obstacle_placements.len().try_into().unwrap(),
+    ))
+}
+
+fn does_guard_loop(grid: &CharGrid, extra_obstacle: CharGridIndexRC) -> bool {
+    let mut guard_original_pos = CharGridIndexRC::zero();
+    for (pos, char) in grid.enumerate_chars_rc() {
+        if char == '^' {
+            guard_original_pos = pos;
+            break;
+        }
+    }
+
+    let mut guard_pos = guard_original_pos;
+    let mut guard_direction = RCDirection::up();
+    let mut guard_visited_positions_directions = HashSet::new();
+
+    while grid.is_in_bounds(guard_pos) {
+        let is_new_position =
+            guard_visited_positions_directions.insert((guard_pos, guard_direction));
+        if !is_new_position {
+            // we've looped
+            return true;
+        }
+        let mut next_pos;
+        loop {
+            next_pos = guard_pos + guard_direction;
+            if grid.index_opt(next_pos) == Some('#') || next_pos == extra_obstacle {
+                guard_direction = guard_direction.clockwise();
+            } else {
+                break;
+            }
+        }
+        guard_pos = next_pos;
+    }
+    // escaped a loop
+    false
 }
 
 #[test]
@@ -65,6 +110,6 @@ fn test_example1() -> Result<()> {
     let (part1, part2) = solve_for(input)?;
 
     assert_eq!(part1, 41);
-    assert_eq!(part2, 0);
+    assert_eq!(part2, 6);
     Ok(())
 }
