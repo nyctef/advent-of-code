b008051360dd7ded569d9fb8cc6970bf543a138f
Mark Jordan <Mark.Jordan@red-gate.com>
2024-12-11 13:09:27 +0000

    experimenting with HashMap vs FxHashMap

--- 2024-rust/src/bin/day11.rs
+++ 2024-rust/src/bin/day11.rs
@@ -1,30 +1,34 @@
 use aoc_2024_rust::util::*;
 use color_eyre::eyre::Result;
-use rustc_hash::FxHashMap as HashMap;
+use itertools::Itertools;
+use rustc_hash::FxHashMap;
+use std::collections::HashMap;
+// use rustc_hash::FxHashMap as HashMap;
 
 pub fn main() -> Result<()> {
     color_eyre::install()?;
 
     let input = get_input(2024, 11)?;
 
-    let (part1, part2) = solve_for(&input)?;
+    let (part1, part2, thousand) = solve_for(&input)?;
 
-    println!("Part 1: {} | Part 2: {}", part1, part2);
+    println!("Part 1: {} | Part 2: {} | 1000: {}", part1, part2, thousand);
     Ok(())
 }
 
-fn solve_for(input: &str) -> Result<(usize, usize)> {
+fn solve_for(input: &str) -> Result<(usize, usize, usize)> {
     let sequence = all_numbers_u64(input.trim());
 
-    let mut cache = HashMap::default();
+    let mut cache = FxHashMap::default();
     let part1 = blink_memoized_seq(&mut cache, &sequence, 25);
     let part2 = blink_memoized_seq(&mut cache, &sequence, 75);
+    let thousand = blink_memoized_seq(&mut cache, &sequence, 1000);
 
-    Ok((part1, part2))
+    Ok((part1, part2, thousand))
 }
 
-fn blink_memoized_seq(
-    cache: &mut HashMap<(usize, u64), usize>,
+fn blink_memoized_seq<T: std::hash::BuildHasher>(
+    cache: &mut HashMap<(usize, u64), usize, T>,
     sequence: &[u64],
     target_num_steps: usize,
 ) -> usize {
@@ -35,8 +39,8 @@ fn blink_memoized_seq(
     result
 }
 
-fn blink_memoized_stone(
-    cache: &mut HashMap<(usize, u64), usize>,
+fn blink_memoized_stone<T: std::hash::BuildHasher>(
+    cache: &mut HashMap<(usize, u64), usize, T>,
     stone: u64,
     target_num_steps: usize,
 ) -> usize {
