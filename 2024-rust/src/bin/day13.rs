393cb1b0a810e511b115c375e752ff5a0f637f54
Nyctef <nyctef@nyctef.com>
2024-12-13 06:19:26 +0000

    some cleanup

--- 2024-rust/src/bin/day13.rs
+++ 2024-rust/src/bin/day13.rs
@@ -1,22 +1,20 @@
-use std::collections::VecDeque;
-
 use aoc_2024_rust::util::*;
 use color_eyre::eyre::Result;
 use itertools::Itertools;
-use rustc_hash::FxHashSet;
 
 pub fn main() -> Result<()> {
     color_eyre::install()?;
 
     let input = get_input(2024, 13)?;
 
-    let (part1, part2) = solve_for(&input)?;
+    let part1 = solve_for(&input, false)?;
+    let part2 = solve_for(&input, true)?;
 
     println!("Part 1: {} | Part 2: {}", part1, part2);
     Ok(())
 }
 
-fn solve_for(input: &str) -> Result<(u64, u64)> {
+fn solve_for(input: &str, part2: bool) -> Result<isize> {
     let machines = input
         .trim()
         .split("\n\n")
@@ -31,33 +29,32 @@ fn solve_for(input: &str) -> Result<(u64, u64)> {
                 lines[2][1].try_into().unwrap(),
             );
 
-            target = target + RCDirection::new(10000000000000, 10000000000000);
+            if part2 {
+                target = target + RCDirection::new(10000000000000, 10000000000000);
+            }
 
             (a, b, target)
         })
         .collect_vec();
 
-    let mut part1 = 0;
-
-    let mut m = 0;
+    let mut total_cost = 0;
 
     for (a_move, b_move, target) in machines {
         let b = ((target.col as isize * a_move.rowdiff) - (a_move.coldiff * target.row as isize))
             / ((b_move.coldiff * a_move.rowdiff) - (b_move.rowdiff * a_move.coldiff));
         let a = (target.col as isize - (b * b_move.coldiff)) / (a_move.coldiff);
 
-        m += 1;
-
+        // we found a solution to the equation, but we're not guaranteed that it's a valid
+        // solution when we're limited to integer values (no half A presses allowed) so
+        // check whether we actually land on the target or not:
         let hits = CharGridIndexRC::zero() + (a_move * a) + (b_move * b) == target;
-        
-        if (hits) {
-        part1 += (3*a) + b;
+
+        if hits {
+            total_cost += (3 * a) + b;
         }
-        dbg!(m, a, b, (3*a) + b, hits);
     }
 
-    let part2 = 0;
-    Ok((part1 as u64, part2))
+    Ok(total_cost)
 }
 
 #[test]
@@ -79,9 +76,8 @@ Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279
 "###;
-    let (part1, part2) = solve_for(input)?;
+    let result = solve_for(input, false)?;
 
-    assert_eq!(part1, 480);
-    assert_eq!(part2, 0);
+    assert_eq!(result, 480);
     Ok(())
 }
