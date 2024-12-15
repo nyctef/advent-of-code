07b634feee86509256afde09a823397d5c5d7996
Nyctef <nyctef@nyctef.com>
2024-12-15 07:08:11 +0000

    more part 2 wip

--- 2024-rust/src/bin/day15.rs
+++ 2024-rust/src/bin/day15.rs
@@ -30,11 +30,11 @@ fn solve_for(input: &str) -> Result<(usize, usize)> {
 
     let mut robot_pos = find_robot(&grid);
 
-    for &movement in &movements {
-        if try_move(&mut grid, robot_pos, movement) {
-            robot_pos = robot_pos + movement;
-        }
-    }
+    // for &movement in &movements {
+    //     if do_move(&mut grid, robot_pos, movement) {
+    //         robot_pos = robot_pos + movement;
+    //     }
+    // }
 
     let gps = grid
         .enumerate_chars_rc()
@@ -74,7 +74,8 @@ fn solve_for(input: &str) -> Result<(usize, usize)> {
         dbg!(&grid);
         eprintln!("move: {}", movement);
         if could_move(&grid, robot_pos, movement) {
-            assert!(try_move(&mut grid, robot_pos, movement));
+            assert!(do_move(&mut grid, robot_pos, movement));
+            eprintln!("moving");
             robot_pos = robot_pos + movement;
         }
         debug_assert_eq!(find_robot(&grid), robot_pos);
@@ -100,7 +101,7 @@ fn find_robot(grid: &CharGrid) -> CharGridIndexRC {
 fn could_move(grid: &CharGrid, pos: CharGridIndexRC, dir: RCDirection) -> bool {
     let could_move_one = |p, d| {
         let tp = p + d;
-        if grid[p] == '#' {
+        if grid[tp] == '#' {
             false
         } else if grid[tp] == '[' || grid[tp] == ']' {
             could_move(grid, tp, d)
@@ -146,21 +147,75 @@ fn could_move(grid: &CharGrid, pos: CharGridIndexRC, dir: RCDirection) -> bool {
     }
 }
 
-fn try_move(grid: &mut CharGrid, pos: CharGridIndexRC, dir: RCDirection) -> bool {
+fn do_move_one(g: &mut CharGrid, p: CharGridIndexRC, d: RCDirection) -> bool {
+    let tp = p + d;
+    eprintln!(
+        "[tm] p {} d {} tp {} g[p] {} g[tp] {}",
+        p, d, tp, g[p], g[tp]
+    );
+    if g[tp] == '#' {
+        panic!("tried to move into '#' when it shouldn't be possible");
+    } else if g[tp] == '[' || g[tp] == ']' {
+        do_move(g, tp, d);
+    } else if g[tp] == '.' {
+        // continue
+    } else {
+        panic!("didn't have empty space to move into");
+    };
+    eprintln!("[tm] y");
+    eprintln!("[tm] moving {} at {} to {}", g[p], p, tp);
+    g.set_index_rc(tp, g[p]);
+    g.set_index_rc(p, '.');
+    true
+}
+
+fn do_move(grid: &mut CharGrid, pos: CharGridIndexRC, dir: RCDirection) -> bool {
     let target_pos = pos + dir;
+    eprintln!(
+        "[tm] pos {} dir {} target {} to_move {}",
+        pos, dir, target_pos, grid[pos]
+    );
     if !grid.is_in_bounds(target_pos) {
         // should be prevented by `#` borders
-        panic!("tried to move off the edge");
+        panic!("tried to move off the edge at {} {}", pos, dir);
     }
+
     if grid[target_pos] == '#' {
-        false
-    } else if grid[target_pos] == '.' || try_move(grid, target_pos, dir) {
-        grid.set_index_rc(target_pos, grid[pos]);
-        grid.set_index_rc(pos, '.');
-        true
-    } else {
-        false
+        panic!("tried to move into '#' when it shouldn't be possible");
     }
+
+    if grid[pos] == '[' || grid[pos] == ']' {
+        if dir == RCDirection::left() || dir == RCDirection::right() {
+            // these behave the same as otherwise
+            do_move(grid, target_pos, dir);
+            assert!(do_move_one(grid, pos, dir));
+        } else {
+            // up or down: we need to treat the [ and ] as coupled
+            // find the other pair:
+            let other_pos = if grid[pos] == '[' {
+                pos + RCDirection::right()
+            } else {
+                pos + RCDirection::left()
+            };
+
+            eprintln!(
+                "[tm] box {} : checking both pos {} and other_pos {}",
+                grid[pos], pos, other_pos
+            );
+
+            assert!(do_move_one(grid, pos, dir));
+            assert!(do_move_one(grid, other_pos, dir));
+        }
+    } else {
+        eprintln!("[tm] moving normally {} {}", pos, grid[pos]);
+        assert!(do_move_one(grid, pos, dir));
+    }
+
+    // eprintln!("[tm] moving {} at {} to {}", grid[pos], pos, target_pos);
+    // grid.set_index_rc(target_pos, grid[pos]);
+    // grid.set_index_rc(pos, '.');
+
+    true
 }
 
 #[test]
