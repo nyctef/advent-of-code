6ef09a17d92f21750fa5601fdd586a117af91b97
Nyctef <nyctef@nyctef.com>
2024-12-18 07:10:38 +0000

    a nicer solution for part 2

--- 2024-rust/src/bin/day18.rs
+++ 2024-rust/src/bin/day18.rs
@@ -1,23 +1,27 @@
+use std::collections::VecDeque;
+
 use aoc_2024_rust::util::*;
 use color_eyre::eyre::Result;
 use derive_more::Constructor;
 use itertools::Itertools;
+use rustc_hash::FxHashSet;
 
 pub fn main() -> Result<()> {
     color_eyre::install()?;
 
     let input = get_input(2024, 18)?;
 
-    let (part1, part2) = solve_for(&input, 70, 70, 1024)?;
+    let part1 = part1(&input, 70, 70, 1024)?;
+    let part2 = part2(&input, 70, 70, 1024)?;
 
-    println!("Part 1: {} | Part 2: {:?}", part1, part2);
+    println!("Part 1: {} | Part 2: {}", part1, part2);
     Ok(())
 }
 
 #[derive(Constructor, Debug, Copy, Clone, Eq, PartialEq, Hash)]
 struct Pos {
-    x: usize,
-    y: usize,
+    x: isize,
+    y: isize,
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
@@ -47,60 +51,110 @@ impl Ord for State {
     }
 }
 
-fn solve_for(input: &str, width: usize, height: usize, count: usize) -> Result<(usize, Pos)> {
+fn part1(input: &str, width: isize, height: isize, count: usize) -> Result<usize> {
     let bytes = input
         .trim()
         .lines()
-        .map(all_numbers_usize)
+        .map(all_numbers_isize)
         .map(|ns| Pos::new(ns[0], ns[1]))
         .collect_vec();
 
-    let mut part1 = 0;
-    let mut part2 = Pos::new(0, 0);
-    for num_bytes in count..bytes.len() {
-        let bytes = bytes.iter().copied().take(num_bytes).collect_vec();
+    // just take first n for part 1
+    let bytes = bytes.into_iter().take(count).collect_vec();
 
-        // eprintln!("adding byte {:?}", bytes[bytes.len() - 1]);
+    let mut search = Dijkstra::new(|s: &State| s.pos);
+    search.push(State {
+        pos: Pos::new(0, 0),
+        steps: 0,
+    });
+    let get_neighbors = |s: State| {
+        let mut nexts = Vec::with_capacity(4);
+        if s.pos.x > 0 {
+            nexts.push(s.next(Pos::new(s.pos.x - 1, s.pos.y)));
+        }
+        if s.pos.y > 0 {
+            nexts.push(s.next(Pos::new(s.pos.x, s.pos.y - 1)));
+        }
+        if s.pos.x < width {
+            nexts.push(s.next(Pos::new(s.pos.x + 1, s.pos.y)));
+        }
+        if s.pos.y < height {
+            nexts.push(s.next(Pos::new(s.pos.x, s.pos.y + 1)));
+        }
 
-        let mut search = Dijkstra::new(|s: &State| s.pos);
-        search.push(State {
-            pos: Pos::new(0, 0),
-            steps: 0,
-        });
-        let get_neighbors = |s: State| {
-            let mut nexts = Vec::with_capacity(4);
-            if s.pos.x > 0 {
-                nexts.push(s.next(Pos::new(s.pos.x - 1, s.pos.y)));
+        nexts = nexts
+            .into_iter()
+            .filter(|s| !bytes.contains(&s.pos))
+            .collect_vec();
+
+        nexts
+    };
+    let res = search.run_single(get_neighbors, |&s| s.pos.x == width && s.pos.y == height);
+
+    let part1 = res.unwrap().steps;
+    Ok(part1)
+}
+
+fn part2(input: &str, width: isize, height: isize, initial_count: usize) -> Result<String> {
+    let input_bytes = input
+        .trim()
+        .lines()
+        .map(all_numbers_isize)
+        .map(|ns| Pos::new(ns[0], ns[1]))
+        .collect_vec();
+
+    for num_bytes in initial_count..input_bytes.len() {
+        let bytes = FxHashSet::from_iter(input_bytes.iter().copied().take(num_bytes));
+
+        let newest_byte = input_bytes[num_bytes - 1];
+        // eprintln!("newest {:?}", newest_byte);
+
+        // flood fill from newest byte to see if we create an unbroken chain spanning the field
+        let mut search = VecDeque::new();
+        let mut seen = FxHashSet::default();
+        search.push_front(newest_byte);
+        while let Some(pos @ Pos { x, y }) = search.pop_front() {
+            // eprintln!("len {} seen {} pos {:?}", search.len(), seen.len(), pos);
+            if !seen.insert(pos) {
+                continue;
             }
-            if s.pos.y > 0 {
-                nexts.push(s.next(Pos::new(s.pos.x, s.pos.y - 1)));
-            }
-            if s.pos.x < width {
-                nexts.push(s.next(Pos::new(s.pos.x + 1, s.pos.y)));
-            }
-            if s.pos.y < height {
-                nexts.push(s.next(Pos::new(s.pos.x, s.pos.y + 1)));
+            if x == 0 || x == width || y == 0 || y == width {
+                let is_touching_left_edge = seen.iter().any(|p| p.x == 0);
+                let is_touching_top_edge = seen.iter().any(|p| p.y == 0);
+                let is_touching_bottom_edge = seen.iter().any(|p| p.y == height);
+                let is_touching_right_edge = seen.iter().any(|p| p.x == width);
+
+                if (is_touching_top_edge && is_touching_left_edge)
+                    || (is_touching_top_edge && is_touching_bottom_edge)
+                    || (is_touching_right_edge && is_touching_bottom_edge)
+                    || (is_touching_right_edge && is_touching_left_edge)
+                {
+                    return Ok(format!("{},{}", newest_byte.x, newest_byte.y));
+                }
             }
 
-            nexts = nexts
-                .into_iter()
-                .filter(|s| !bytes.contains(&s.pos))
-                .collect_vec();
+            let nexts = vec![
+                Pos::new(x + 1, y + 1),
+                Pos::new(x + 0, y + 1),
+                Pos::new(x - 1, y + 1),
+                Pos::new(x - 1, y + 0),
+                Pos::new(x - 1, y - 1),
+                Pos::new(x - 0, y - 1),
+                Pos::new(x + 1, y - 1),
+                Pos::new(x + 1, y - 0),
+            ];
 
-            // eprintln!("s {:?} | {:?} ", s, nexts);
-
-            nexts
-        };
-        let res = search.run_single(get_neighbors, |&s| s.pos.x == width && s.pos.y == height);
-        if res.is_none() {
-            part2 = bytes[bytes.len() - 1];
-            break;
-        } else if part1 == 0 {
-            part1 = res.unwrap().steps
+            for n in nexts {
+                if !bytes.contains(&n) {
+                    continue;
+                } else {
+                    search.push_front(n);
+                }
+            }
         }
     }
 
-    Ok((part1, part2))
+    panic!("couldn't find a part 2 solution");
 }
 
 #[test]
@@ -132,9 +186,10 @@ fn test_example1() -> Result<()> {
 1,6
 2,0
 "###;
-    let (part1, part2) = solve_for(input, 6, 6, 12)?;
+    let part1 = part1(input, 6, 6, 12)?;
+    let part2 = part2(input, 6, 6, 12)?;
 
     assert_eq!(part1, 22);
-    assert_eq!(part2, 0);
+    assert_eq!(part2, "6,1");
     Ok(())
 }
