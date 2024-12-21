a1a47d58976569be9e16784b895ec0ad3f10ce7a
Nyctef <nyctef@nyctef.com>
2024-12-21 06:49:06 +0000

    day 21 attempt 1

--- 2024-rust/src/bin/day21.rs
+++ 2024-rust/src/bin/day21.rs
@@ -1,5 +1,4 @@
 use std::iter;
-
 use aoc_2024_rust::util::*;
 use color_eyre::eyre::Result;
 use itertools::Itertools;
@@ -15,7 +14,7 @@ pub fn main() -> Result<()> {
     Ok(())
 }
 
-fn solve_for(input: &str) -> Result<(u64, u64)> {
+fn solve_for(input: &str) -> Result<(usize, u64)> {
     let codes = input.trim().lines().collect_vec();
 
     let keypad = CharGrid::from_string(
@@ -35,7 +34,9 @@ fn solve_for(input: &str) -> Result<(u64, u64)> {
     );
     let directions_start = directions.find_single_char('A');
 
+    let mut part1 = 0;
     for code in codes {
+        eprintln!("code: {:?}", code);
         let mut first_robot_pos = keypad_start;
         let mut first_robot_moves = vec![];
         for code_char in code.chars() {
@@ -55,10 +56,58 @@ fn solve_for(input: &str) -> Result<(u64, u64)> {
             first_robot_moves.push('A');
             first_robot_pos = target;
         }
-        dbg!(&first_robot_moves);
+        eprintln!("first robot: {:?}", first_robot_moves);
+
+        let mut second_robot_pos = directions_start;
+        let mut second_robot_moves = vec![];
+        for &code_char in &first_robot_moves {
+
+            let target = directions.find_single_char(code_char);
+            let dist = RCDirection::from_to(second_robot_pos, target);
+            if dist.coldiff < 0 {
+                second_robot_moves.extend(iter::repeat_n('<', dist.coldiff.abs() as usize));
+            } else if dist.coldiff > 0 {
+                second_robot_moves.extend(iter::repeat_n('>', dist.coldiff.abs() as usize));
+            }
+            if dist.rowdiff < 0 {
+                second_robot_moves.extend(iter::repeat_n('^', dist.rowdiff.abs() as usize));
+            } else if dist.rowdiff > 0 {
+                second_robot_moves.extend(iter::repeat_n('v', dist.rowdiff.abs() as usize));
+            }
+
+            second_robot_moves.push('A');
+            second_robot_pos = target;
+        }
+        eprintln!("second robot: {:?}", second_robot_moves);
+
+        let mut third_robot_pos = directions_start;
+        let mut third_robot_moves = vec![];
+        for &code_char in &second_robot_moves {
+
+            let target = directions.find_single_char(code_char);
+            let dist = RCDirection::from_to(third_robot_pos, target);
+            if dist.coldiff < 0 {
+                third_robot_moves.extend(iter::repeat_n('<', dist.coldiff.abs() as usize));
+            } else if dist.coldiff > 0 {
+                third_robot_moves.extend(iter::repeat_n('>', dist.coldiff.abs() as usize));
+            }
+            if dist.rowdiff < 0 {
+                third_robot_moves.extend(iter::repeat_n('^', dist.rowdiff.abs() as usize));
+            } else if dist.rowdiff > 0 {
+                third_robot_moves.extend(iter::repeat_n('v', dist.rowdiff.abs() as usize));
+            }
+
+            third_robot_moves.push('A');
+            third_robot_pos = target;
+        }
+        eprintln!("third robot: {:?}", third_robot_moves);
+
+        let complexity = third_robot_moves.len() * code.trim_matches('A').parse::<usize>().unwrap();
+        eprintln!("complexity: {}", complexity);
+        part1 += complexity;
+
     }
 
-    let part1 = 0;
     let part2 = 0;
     Ok((part1, part2))
 }
