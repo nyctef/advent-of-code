7b082b1e24870bcd18cd56a12ef38de6a33137f5
Nyctef <nyctef@nyctef.com>
2024-12-23 08:16:21 +0000

    try and explain this algorithm copied from wikipedia

--- 2024-rust/src/bin/day23.rs
+++ 2024-rust/src/bin/day23.rs
@@ -94,6 +94,57 @@ fn solve_for(input: &str) -> (usize, String) {
     (part1, part2)
 }
 
+/// https://en.wikipedia.org/wiki/Bron%E2%80%93Kerbosch_algorithm
+///
+/// Bron-Kerbosch is an algorithm for finding "cliques" (fully-connected subgraphs).
+///
+/// In particular Bron_kerbosch finds all "maximal" cliques - cliques that could not be expanded
+/// any further by adding a neighboring vertex. For today's puzzle, we care about the largest
+/// maximal clique - called the "maximum" clique, so we'll look through the results to find that
+/// once we're done with this algorithm.
+///
+/// A simpler algorithm for finding maximal cliques could look like the following:
+/// - for each vertex in the graph
+///   - put that vertex into a set R
+///   - loop:
+///     - for all neighbors of vertices in R
+///       - if a neighbor added to R would keep R as a clique, then add it and loop
+///       - otherwise, stop and report R as a clique
+///
+/// Bron-Kerbosch is like that, but a bit smarter:
+///
+/// - R is a set of vertices currently known to be in a clique
+/// - P is a set of candidate vertices that could be added next
+/// - X is a set of ignored vertices that can't be part of the clique
+///
+/// The algorithm starts with R and X empty, and all the graph vertices in P. Since we begin with
+/// zero clique members, we can add any vertex to begin the clique. This vertex moves from P into
+/// R, and we prepare to recurse. In order to find a second vertex to add, we need to only consider
+/// vertices that are neighbors of the one we've just picked. We take the current P value and
+/// filter it to only include immediate neighbors of the starting vertex.
+///
+/// After one recursion, we have:
+///
+/// - R is a set of vertices (just our starting vertex)
+/// - P is a set of candidate vertices that could be added next (all neighbors of R)
+/// - X is empty since we haven't added anything to it yet
+///
+/// So in a degenerate case (if P is now empty) then we just have a clique of one vertex, and we
+/// report that. Otherwise, we iterate through candidates in our new candidate set and find the
+/// neighbors of each second vertex. We set the next P value to be the intersection of the current
+/// P (neighbors of the first vertex) and the second P (neighbors of the second) since any
+/// candidate for a third vertex must be neighbors of both. Like the previous more naive algorithm,
+/// this continues until there are no more candidates (new vertices that are neighbors of every
+/// existing vertex) at which point we can report a clique.
+///
+/// Finally, we need to make sure that we don't report the same cliques over and over when starting
+/// from different vertices within them. After recursing, we remove the current candidate vertex
+/// from P so that it's not included as a candidate in future excursions at that level. Also, the
+/// algorithm tracks a third set X which includes the list of candidates that have been processed.
+/// We don't add a clique to the result if a vertex in X is a neighbor of the current candidate.
+/// This ensures (?) that just because we've removed a processed vertex from P, we don't then just
+/// carry on and report the sub-clique which excludes that vertex.
+///
 fn bron_kerbosch<'i>(
     r: FxHashSet<&'i str>,
     mut p: FxHashSet<&'i str>,
@@ -105,7 +156,7 @@ fn bron_kerbosch<'i>(
         result.push(Vec::from_iter(r.iter().copied()));
     }
 
-    // since we need to mutate P inside the loop
+    // take a copy of P since we mutate it inside the loop
     let candidates = p.iter().copied().collect_vec();
     for v in candidates {
         let neighbors = FxHashSet::from_iter(connections[v].iter().copied());
