d10f68272ed67c73561be5f5dfcf1de93d21fb88
Nyctef <nyctef@nyctef.com>
2024-12-23 07:37:38 +0000

    day 23 part 2

--- 2024-rust/src/bin/day23.rs
+++ 2024-rust/src/bin/day23.rs
@@ -1,7 +1,7 @@
 use aoc_2024_rust::util::*;
 use color_eyre::eyre::Result;
 use itertools::Itertools;
-use rustc_hash::FxHashSet;
+use rustc_hash::{FxHashMap, FxHashSet};
 
 pub fn main() -> Result<()> {
     color_eyre::install()?;
@@ -14,7 +14,7 @@ pub fn main() -> Result<()> {
     Ok(())
 }
 
-fn solve_for(input: &str) -> (usize, u64) {
+fn solve_for(input: &str) -> (usize, String) {
     let mut computers = FxHashSet::default();
     let mut connections = FxHashSet::default();
     let mut triples = FxHashSet::default();
@@ -33,7 +33,6 @@ fn solve_for(input: &str) -> (usize, u64) {
     for &c1 in &computers {
         for &c2 in &computers {
             for &c3 in &computers {
-
                 if !(c1.starts_with('t') || c2.starts_with('t') || c3.starts_with('t')) {
                     continue;
                 }
@@ -51,19 +50,76 @@ fn solve_for(input: &str) -> (usize, u64) {
                 {
                     let mut triple = vec![c1, c2, c3];
                     triple.sort();
-                    eprintln!("found triple {:?}", triple);
+                    // eprintln!("found triple {:?}", triple);
                     triples.insert(triple);
                 }
             }
         }
     }
 
-    eprintln!("triples {:?}", triples);
+    // eprintln!("triples {:?}", triples);
 
-    let mut part1 = triples.len();
-    let mut part2 = 0;
+    let mut connection_map = FxHashMap::default();
+    for conn in connections {
+        connection_map
+            .entry(conn[0])
+            .or_insert(vec![])
+            .push(conn[1]);
+        connection_map
+            .entry(conn[1])
+            .or_insert(vec![])
+            .push(conn[0]);
+    }
+
+    let part1 = triples.len();
+
+    let mut result = vec![];
+    bron_kerbosch(
+        FxHashSet::default(),
+        computers.clone(),
+        FxHashSet::default(),
+        &connection_map,
+        &mut result,
+    );
+
+    let mut largest = result
+        .into_iter()
+        .max_by(|a, b| a.len().cmp(&b.len()))
+        .unwrap();
+
+    largest.sort();
+
+    let part2 = largest.iter().join(",");
 
     (part1, part2)
+}
+
+fn bron_kerbosch<'i>(
+    r: FxHashSet<&'i str>,
+    mut p: FxHashSet<&'i str>,
+    mut x: FxHashSet<&'i str>,
+    connections: &FxHashMap<&'i str, Vec<&'i str>>,
+    result: &mut Vec<Vec<&'i str>>,
+) {
+    if p.is_empty() && x.is_empty() && r.len() > 3 {
+        result.push(Vec::from_iter(r.iter().copied()));
+    }
+
+    // since we need to mutate P inside the loop
+    let candidates = p.iter().copied().collect_vec();
+    for v in candidates {
+        let neighbors = FxHashSet::from_iter(connections[v].iter().copied());
+
+        let mut next_r = r.clone();
+        next_r.insert(v);
+        let next_p = &p & &neighbors;
+        let next_x = &x & &neighbors;
+
+        bron_kerbosch(next_r, next_p, next_x, connections, result);
+
+        p.remove(v);
+        x.insert(v);
+    }
 }
 
 #[test]
@@ -105,5 +161,5 @@ td-yn
     let (part1, part2) = solve_for(input);
 
     assert_eq!(part1, 7);
-    assert_eq!(part2, 0);
+    assert_eq!(part2, "co,de,ka,ta");
 }
