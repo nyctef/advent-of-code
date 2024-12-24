ce38da67b2b0966e227fbb90663aa5926c073d58
Mark Jordan <Mark.Jordan@red-gate.com>
2024-12-24 10:45:38 +0000

    faster cycle detection for day 6

--- 2024-rust/src/bin/day06.rs
+++ 2024-rust/src/bin/day06.rs
@@ -32,16 +32,12 @@ fn solve_for(input: &str) -> Result<(u64, u64)> {
 
     while grid.is_in_bounds(guard_pos) {
         guard_visited_positions.insert(guard_pos);
-        let mut next_pos;
-        loop {
-            next_pos = guard_pos + guard_direction;
-            if grid.index_opt(next_pos) == Some('#') {
-                guard_direction = guard_direction.clockwise();
-            } else {
-                break;
-            }
-        }
-        guard_pos = next_pos;
+        move_guard(
+            &grid,
+            CharGridIndexRC::new(grid.width() + 1, grid.height() + 1),
+            &mut guard_pos,
+            &mut guard_direction,
+        );
     }
 
     eprintln!("part 1 took {}ms", part1_timer.elapsed().as_millis());
@@ -60,36 +56,44 @@ fn solve_for(input: &str) -> Result<(u64, u64)> {
 }
 
 fn does_guard_loop(grid: &CharGrid, extra_obstacle: CharGridIndexRC) -> bool {
-    let (guard_original_pos, _) = grid
-        .enumerate_chars_rc()
-        .filter(|&(_, c)| c == '^')
-        .exactly_one()
-        .unwrap();
+    let guard_original_pos = grid.find_single_char('^');
 
-    let mut guard_pos = guard_original_pos;
-    let mut guard_direction = RCDirection::up();
-    let mut guard_visited_positions_directions = FxHashSet::default();
+    // https://en.wikipedia.org/wiki/Cycle_detection#Tortoise_and_hare
 
-    while grid.is_in_bounds(guard_pos) {
-        let is_new_position =
-            guard_visited_positions_directions.insert((guard_pos, guard_direction));
-        if !is_new_position {
-            // we've looped
+    let mut tortoise_pos = guard_original_pos;
+    let mut tortoise_dir = RCDirection::up();
+    let mut hare_pos = guard_original_pos;
+    let mut hare_dir = RCDirection::up();
+
+    while grid.is_in_bounds(tortoise_pos) && grid.is_in_bounds(hare_pos) {
+        move_guard(grid, extra_obstacle, &mut tortoise_pos, &mut tortoise_dir);
+        move_guard(grid, extra_obstacle, &mut hare_pos, &mut hare_dir);
+        move_guard(grid, extra_obstacle, &mut hare_pos, &mut hare_dir);
+
+        if hare_pos == tortoise_pos && hare_dir == tortoise_dir {
             return true;
         }
-        let mut next_pos;
-        loop {
-            next_pos = guard_pos + guard_direction;
-            if grid.index_opt(next_pos) == Some('#') || next_pos == extra_obstacle {
-                guard_direction = guard_direction.clockwise();
-            } else {
-                break;
-            }
-        }
-        guard_pos = next_pos;
     }
     // escaped a loop
     false
+}
+
+fn move_guard(
+    grid: &CharGrid,
+    extra_obstacle: CharGridIndexRC,
+    guard_pos: &mut CharGridIndexRC,
+    guard_direction: &mut RCDirection,
+) {
+    let mut next_pos;
+    loop {
+        next_pos = *guard_pos + *guard_direction;
+        if grid.index_opt(next_pos) == Some('#') || next_pos == extra_obstacle {
+            *guard_direction = guard_direction.clockwise();
+        } else {
+            break;
+        }
+    }
+    *guard_pos = next_pos;
 }
 
 #[test]
