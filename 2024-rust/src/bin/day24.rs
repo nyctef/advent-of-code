296d869c4cea3fb2984b093370a865f9aba8d53a
Nyctef <nyctef@nyctef.com>
2024-12-24 07:00:00 +0000

    part 2 wip

--- 2024-rust/src/bin/day24.rs
+++ 2024-rust/src/bin/day24.rs
@@ -31,58 +31,90 @@ fn solve_for(input: &str) -> (u64, u64) {
     });
     let gates = FxHashMap::from_iter(gates);
 
+    // dbg!(&gates.iter().sorted().collect_vec());
+
+    for i in 0..45 {
+        let mut log = vec![];
+        let n = 2_u64.pow(i);
+        let result = run_circuit(n, n, &gates, &mut log);
+        if result != n * 2 {
+            eprintln!();
+            eprintln!("adding {} and {} failed: expected\n{:#045b} but got\n{:#045b} instead", n, n, n*2, result);
+            // for log_line in log {
+            //     eprintln!("{}", log_line);
+            // }
+        }
+    }
+
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    (part1, part2)
+}
+
+fn run_circuit(
+    mut x: u64,
+    mut y: u64,
+    gates: &FxHashMap<&str, (&str, &str, &str)>,
+    log: &mut Vec<String>,
+) -> u64 {
     // dbg!(&inputs, &gates);
     //
-    let values = inputs.keys().map(|k| (*k, false));
-    let mut values = FxHashMap::from_iter(values);
-    for (wire, value) in inputs {
-        values.entry(wire).insert_entry(value);
+    let mut values = FxHashMap::default();
+    for i in 0..45 {
+        let x_val = x & 1;
+        x >>= 1;
+        values.entry(format!("x{:02}", i)).insert_entry(x_val);
+        let y_val = y & 1;
+        y >>= 1;
+        values.entry(format!("y{:02}", i)).insert_entry(y_val);
     }
-     // dbg!(&values.iter().sorted().collect_vec());
+
+    // dbg!(&values);
+    // dbg!(&values.iter().sorted().collect_vec());
 
     'outer: loop {
-
-        for (wire, gate) in &gates {
+        for (wire, gate) in gates {
             let &(l, op, r) = gate;
 
-            if !values.contains_key(wire) && values.contains_key(l) && values.contains_key(r) {
+            if !values.contains_key(*wire) && values.contains_key(l) && values.contains_key(r) {
                 let l_value = values[l];
                 let r_value = values[r];
                 let new_value = match op {
-                    "AND" => l_value && r_value,
-                    "OR" => l_value || r_value,
+                    "AND" => l_value & r_value,
+                    "OR" => l_value | r_value,
                     "XOR" => l_value ^ r_value,
                     _ => panic!("unknown operation {}", op),
                 };
 
-                eprintln!("{} ({}) {} {} ({}) -> {} ({})", l, l_value, op, r, r_value, wire, new_value);
+                log.push(format!(
+                    "{} ({}) {} {} ({}) -> {} ({})",
+                    l, l_value, op, r, r_value, wire, new_value
+                ));
 
-                values.entry(wire).insert_entry(new_value);
+                values.entry(wire.to_string()).insert_entry(new_value);
 
                 continue 'outer;
-
             }
-
         }
 
         break;
     }
 
-    let z_values = values.iter().filter(|(k, v)| k.starts_with('z')).sorted().map(|(k, v)| v);
+    let z_values = values
+        .iter()
+        .filter(|(k, v)| k.starts_with('z'))
+        .sorted()
+        .map(|(k, v)| v);
     // dbg!(&z_values);
 
     let mut part1 = 0;
     let mut p2 = 0;
     for z in z_values {
-        let z = if *z { 1_u64 } else { 0 };
         part1 += z * 2_u64.pow(p2);
         p2 += 1;
-
     }
-
-    let mut part2 = 0;
-
-    (part1, part2)
+    part1
 }
 
 #[test]
