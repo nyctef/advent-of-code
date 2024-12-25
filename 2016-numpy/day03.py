24712fd7d88598241549d5b8b8e14b2d382934e5
Nyctef <nyctef@nyctef.com>
2024-12-25 16:11:52 +0000

    extract shared logic

--- 2016-numpy/day03.py
+++ 2016-numpy/day03.py
@@ -8,22 +8,23 @@ def solve_for(input: str):
 
     triangles = np.loadtxt(input, dtype=int)
 
-    test1 = triangles[:, 0] < triangles[:, 1] + triangles[:, 2]
-    test2 = triangles[:, 1] < triangles[:, 2] + triangles[:, 0]
-    test3 = triangles[:, 2] < triangles[:, 0] + triangles[:, 1]
-    part1 = (test1 & test2 & test3).sum()
+    part1 = count_valid_triangles(triangles)
 
     # reshape(-1, 3, 3): leave first dimension unspecified, and group into 3x3 matrices
     # transpose(0, 2, 1): swap the last two dimensions (make the 3rd dimension the second and vice-versa)
     # reshape(-1, 3): leave first dimension unspecified, and group into 3-element arrays again
     triangles = triangles.reshape(-1, 3, 3).transpose(0, 2, 1).reshape(-1, 3)
 
+    part2 = count_valid_triangles(triangles)
+
+    return (part1, part2)
+
+
+def count_valid_triangles(triangles):
     test1 = triangles[:, 0] < triangles[:, 1] + triangles[:, 2]
     test2 = triangles[:, 1] < triangles[:, 2] + triangles[:, 0]
     test3 = triangles[:, 2] < triangles[:, 0] + triangles[:, 1]
-    part2 = (test1 & test2 & test3).sum()
-
-    return (part1, part2)
+    return (test1 & test2 & test3).sum()
 
 
 def test_example_input():
