e631f008f6e9cb8f8e156022c878bd7e31a7d7d3
Nyctef <nyctef@nyctef.com>
2024-12-25 05:21:11 +0000

    cleanup

--- 2024-rust/src/bin/day25.rs
+++ 2024-rust/src/bin/day25.rs
@@ -7,13 +7,13 @@ pub fn main() -> Result<()> {
 
     let input = get_input(2024, 25)?;
 
-    let (part1, part2) = solve_for(&input);
+    let part1 = solve_for(&input);
 
-    println!("Part 1: {} | Part 2: {}", part1, part2);
+    println!("Part 1: {}", part1);
     Ok(())
 }
 
-fn solve_for(input: &str) -> (u64, u64) {
+fn solve_for(input: &str) -> u64 {
     let items = input.trim().split("\n\n").map(|i| i.trim()).collect_vec();
     let locks = items
         .iter()
@@ -26,40 +26,22 @@ fn solve_for(input: &str) -> (u64, u64) {
         .map(|i| i[5..i.len() - 5].trim())
         .collect_vec();
 
-    let locks = locks
-        .into_iter()
-        .map(|l| {
-            let res = get_heights(l);
-
-            res
-        })
-        .collect_vec();
-    let keys = keys
-        .into_iter()
-        .map(|l| {
-            let res = get_heights(l);
-
-            res
-        })
-        .collect_vec();
+    let locks = locks.into_iter().map(get_heights).collect_vec();
+    let keys = keys.into_iter().map(get_heights).collect_vec();
 
     let mut part1 = 0;
     for lock in locks {
         'next_key: for key in &keys {
             for i in 0..5 {
-                if lock[i] + key[i] >5 {
+                if lock[i] + key[i] > 5 {
                     continue 'next_key;
                 }
-
             }
             part1 += 1;
-
         }
     }
 
-    let mut part2 = 0;
-
-    (part1, part2)
+    part1
 }
 
 fn get_heights(l: &str) -> Vec<i32> {
@@ -76,15 +58,4 @@ fn get_heights(l: &str) -> Vec<i32> {
         res.push(height);
     }
     res
-}
-
-#[test]
-fn test_example1() {
-    let input = r###"
-    
-"###;
-    let (part1, part2) = solve_for(input);
-
-    assert_eq!(part1, 0);
-    assert_eq!(part2, 0);
 }
