7de7b2a3c9530b72fc08cb2c4e1119a1d1af768d
Nyctef <nyctef@nyctef.com>
2024-12-25 11:48:33 +0000

    day 2 part 2

--- 2016-numpy/day02.py
+++ 2016-numpy/day02.py
@@ -39,7 +39,32 @@ def solve_for(input: str):
         part1 += keypad[pos[1], pos[0]]
         # this also works, but is a bit arcane (can it be simplified?)
         # part1 += keypad[tuple(pos[::-1].astype(int))]
+
+    keypad2 = """
+  1  
+ 234 
+56789
+ ABC 
+  D  """.strip(
+        "\n"
+    )
+    keypad2 = np.array([list(row) for row in keypad2.splitlines()])
+
+    print(keypad2)
     part2 = ""
+    pos = np.array([0, 2])
+    for line in instructions:
+        for instr in line:
+            next_pos = pos + instr
+            if not (
+                0 <= next_pos[0] < keypad2.shape[1]
+                and 0 <= next_pos[1] < keypad2.shape[0]
+                and keypad2[next_pos[1], next_pos[0]] != " "
+            ):
+                continue
+            pos = next_pos
+        part2 += keypad2[pos[1], pos[0]]
+
     return (part1, part2)
 
 
@@ -53,7 +78,7 @@ UUUUD
     (part1, part2) = solve_for(example)
 
     assert part1 == "1985"
-    assert part2 == ""
+    assert part2 == "5DB3"
 
 
 if __name__ == "__main__":
