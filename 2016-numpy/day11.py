35438eebcf77358f0ae62c33083ecaa2f6980caf
Mark Jordan <mark.jordan@red-gate.com>
2024-12-27 17:48:20 +0000

    day 11 attempt 1

--- 2016-numpy/day11.py
+++ 2016-numpy/day11.py
@@ -1,9 +1,11 @@
+from dataclasses import astuple, dataclass
 import re
 from util import get_input
 import numpy as np
 from numpy.typing import NDArray
 import heapq
 from copy import deepcopy
+import itertools
 
 
 class MaxHeapObj(object):
@@ -44,6 +46,26 @@ class MaxHeap:
         return len(self.h)
 
 
+class MinHeap:
+    """
+    based on https://stackoverflow.com/a/40455775/895407
+    """
+
+    h = []
+
+    def __init__(self):
+        h = []
+
+    def push(self, val):
+        heapq.heappush(self.h, val)
+
+    def pop(self):
+        return heapq.heappop(self.h)
+
+    def __len__(self):
+        return len(self.h)
+
+
 def score(floors: list):
     # TODO: does the scoring function matter significantly here?
     return 3 * len(floors[3]) + len(floors[2])
@@ -70,8 +92,53 @@ def is_valid(floors):
     return True
 
 
-def next_states(state):
-    (turns, _score, elevator, floors) = state
+@dataclass
+class State:
+    turns: int
+    elevator: int
+    floors: list
+
+    def __lt__(self, other):
+        if self.turns != other.turns:
+            return self.turns < other.turns
+        # return score(self.floors) > score(other.floors)
+        return False
+
+
+def next_states(state: State):
+    (turns, elevator, floors) = astuple(state)
+
+    result = []
+    # from each point on floors[elevator], we can take 1-2 items up or down
+    for item in floors[elevator]:
+        if elevator < len(floors) - 1:
+            next_state_up = deepcopy(floors)
+            next_state_up[elevator].remove(item)
+            next_state_up[elevator + 1].append(item)
+            result.append(State(turns + 1, elevator + 1, next_state_up))
+        if elevator > 0:
+            next_state_down = deepcopy(floors)
+            next_state_down[elevator].remove(item)
+            next_state_down[elevator - 1].append(item)
+            result.append(State(turns + 1, elevator - 1, next_state_down))
+
+    for item1, item2 in itertools.combinations(floors[elevator], 2):
+        if elevator < len(floors) - 1:
+            next_state_up = deepcopy(floors)
+            next_state_up[elevator].remove(item1)
+            next_state_up[elevator].remove(item2)
+            next_state_up[elevator + 1].append(item1)
+            next_state_up[elevator + 1].append(item2)
+            result.append(State(turns + 1, elevator + 1, next_state_up))
+        if elevator > 0:
+            next_state_down = deepcopy(floors)
+            next_state_down[elevator].remove(item1)
+            next_state_down[elevator].remove(item2)
+            next_state_down[elevator - 1].append(item1)
+            next_state_down[elevator - 1].append(item2)
+            result.append(State(turns + 1, elevator - 1, next_state_down))
+
+    return [r for r in result if is_valid(r.floors)]
 
 
 def solve_for(input: str):
@@ -88,9 +155,31 @@ def solve_for(input: str):
     print(floors)
 
     # num turns, score for some kind of heuristic, current floor, floor state
-    state = (0, score(floors), 0, floors)
+    state = State(0, 0, floors)
 
-    part1 = ""
+    fewest_turns = 99999999999
+    search = MinHeap()
+    search.push(state)
+    counter = 1
+    while len(search):
+        current = search.pop()
+
+        if counter % 10_000 == 0:
+            print(
+                f"  {len(search)=} {current.turns=} {score(current.floors)=} {fewest_turns=}"
+            )
+        counter += 1
+
+        if current.turns >= fewest_turns:
+            continue
+        if is_done(current.floors):
+            fewest_turns = current.turns
+            continue
+
+        for next in next_states(current):
+            search.push(next)
+
+    part1 = fewest_turns
     part2 = ""
 
     return (part1, part2)
@@ -102,6 +191,19 @@ def test_floors_valid():
     assert is_valid([["mc A", "gen A"]]) == True
     assert is_valid([["mc A", "gen A", "gen B"]]) == True
     assert is_valid([["mc A", "gen B"]]) == False
+
+
+def test_example_input():
+    example = """
+The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+The second floor contains a hydrogen generator.
+The third floor contains a lithium generator.
+The fourth floor contains nothing relevant.
+"""
+    (part1, part2) = solve_for(example)
+
+    assert part1 == 11
+    assert part2 == ""
 
 
 if __name__ == "__main__":
