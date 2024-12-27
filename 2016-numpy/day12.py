e4f22fe098806f10d2e969dae3dc6b9c15d73923
Mark Jordan <mark.jordan@red-gate.com>
2024-12-27 20:28:13 +0000

    day 12

--- 2016-numpy/day12.py
+++ 2016-numpy/day12.py
@@ -4,15 +4,17 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-def solve_for(input: str):
+def solve_for(input: str, part2: bool):
     lines = input.strip().splitlines()
 
     registers = defaultdict(lambda: 0)
+    if part2:
+        registers["c"] = 1
     pc = 0
     while pc < len(lines) and pc >= 0:
         (instr, x, *y) = lines[pc].split(" ")
         y = y[0] if len(y) else None
-        # print(instr, x, y, registers)
+        # print(instr, x, y, registers.items())
         match instr:
             case "cpy":
                 registers[y] = int(x) if x.isdigit() else registers[x]
@@ -24,15 +26,13 @@ def solve_for(input: str):
                 registers[x] -= 1
                 pc += 1
             case "jnz":
-                if registers[x] != 0:
+                val = int(x) if x.isdigit() else registers[x]
+                if val != 0:
                     pc += int(y)
                 else:
                     pc += 1
 
-    part1 = registers["a"]
-    part2 = ""
-
-    return (part1, part2)
+    return registers["a"]
 
 
 def test_example_input():
@@ -44,12 +44,12 @@ dec a
 jnz a 2
 dec a
 """
-    (part1, part2) = solve_for(example)
+    part1 = solve_for(example, False)
 
     assert part1 == 42
-    assert part2 == ""
 
 
 if __name__ == "__main__":
-    (part1, part2) = solve_for(get_input(2016, 12))
+    part1 = solve_for(get_input(2016, 12), False)
+    part2 = solve_for(get_input(2016, 12), True)
     print(f"Part 1: {part1} | Part 2: {part2}")
