047e746ff598417da12221c26bfb5e9c41dfc2ee
Nyctef <nyctef@nyctef.com>
2024-12-28 22:37:25 +0000

    day 19 part 2 attempt 1

--- 2016-numpy/day19.py
+++ 2016-numpy/day19.py
@@ -19,9 +19,9 @@ def solve_for(input: str):
         while elfs[next_elf_ptr] == 0:
             next_elf_ptr = next_elf(next_elf_ptr)
 
-        print(
-            f"elf {ptr+1} takes {elfs[next_elf_ptr]} presents from elf {next_elf_ptr+1}"
-        )
+        # print(
+        #     f"elf {ptr+1} takes {elfs[next_elf_ptr]} presents from elf {next_elf_ptr+1}"
+        # )
         elfs[ptr] += elfs[next_elf_ptr]
         elfs[next_elf_ptr] = 0
 
@@ -31,17 +31,37 @@ def solve_for(input: str):
 
         ptr = next_elf(ptr)
 
-    part2 = ""
+    print("======")
+
+    elfs = [(1, i + 1) for i in range(elf_count)]
+    ptr = 0
+
+    def next_elf(i):
+        return (i + 1) % len(elfs)
+
+    while len(elfs) > 1:
+        opposite_elf = (ptr + (len(elfs) // 2)) % len(elfs)
+        # print()
+        # print(elfs)
+        # print(f"{ptr=} {len(elfs)=} {opposite_elf=}")
+        (count, i) = elfs[ptr]
+        (opp_count, opp_i) = elfs[opposite_elf]
+        # print(f"elf {i} takes {elfs[opposite_elf][0]} presents from elf {opp_i}")
+        count += opp_count
+        elfs[ptr] = (count, i)
+        del elfs[opposite_elf]
+        if opposite_elf < ptr:
+            ptr -= 1
+        ptr = next_elf(ptr)
+
+    part2 = elfs[0][1]
 
     return (part1, part2)
 
 
 def test_example_input():
-    assert solve_for("5")[0] == 3
-    assert solve_for("7")[0] == 5
-
-    assert part1 == 3
-    assert part2 == ""
+    # assert solve_for("5")[0] == 3
+    assert solve_for("5")[1] == 2
 
 
 if __name__ == "__main__":
