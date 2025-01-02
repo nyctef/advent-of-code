eed62f8782ebb5dcdea9412ba8d50e9e13dff445
Mark Jordan <Mark.Jordan@red-gate.com>
2025-01-02 16:09:30 +0000

    day 1

--- 2017-rust/src/bin/day01.rs
+++ 2017-rust/src/bin/day01.rs
@@ -13,22 +13,35 @@ pub fn main() -> Result<()> {
     Ok(())
 }
 
-fn solve_for(input: &str) -> (u64, u64) {
-    // ...
+fn solve_for(input: &str) -> (usize, usize) {
+    let digits = input
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect_vec();
+
     let mut part1 = 0;
     let mut part2 = 0;
+    for i in 0..digits.len() {
+        if digits[i] == digits[(i + 1) % digits.len()] {
+            part1 += digits[i];
+        }
 
-    todo!();
+        if digits[i] == digits[(i + digits.len() / 2) % digits.len()] {
+            part2 += digits[i];
+        }
+    }
+
     (part1, part2)
 }
 
 #[test]
 fn test_example1() {
     let input = r###"
-    
+1122
 "###;
     let (part1, part2) = solve_for(input);
 
-    assert_eq!(part1, 0);
+    assert_eq!(part1, 3);
     assert_eq!(part2, 0);
 }
