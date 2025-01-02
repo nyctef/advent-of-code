af419ee53e16d8ee5042b3e3e43e67848ccce86e
Nyctef <nyctef@nyctef.com>
2025-01-02 23:12:16 +0000

    day 7 part 2

--- 2017-rust/src/bin/day07.rs
+++ 2017-rust/src/bin/day07.rs
@@ -14,10 +14,11 @@ pub fn main() -> Result<()> {
     Ok(())
 }
 
-fn solve_for(input: &str) -> (&str, u64) {
+fn solve_for(input: &str) -> (&str, u32) {
     let mut part2 = 0;
 
     let mut parents = FxHashMap::default();
+    let mut children = FxHashMap::default();
     let mut weights = FxHashMap::<&str, u32>::default();
     for line in input.trim().lines() {
         if let Some((left, right)) = line.split_once(" -> ") {
@@ -25,20 +26,107 @@ fn solve_for(input: &str) -> (&str, u64) {
             let (below, weight) = left.split_once(" ").unwrap();
             let weight = all_numbers(weight)[0];
             weights.insert(below, weight);
-            for a in above {
+            for a in above.clone() {
                 parents.insert(a, below);
             }
+            children.insert(below, above.collect_vec());
+        } else {
+            let (below, weight) = line.split_once(" ").unwrap();
+            let weight = all_numbers(weight)[0];
+            weights.insert(below, weight);
         }
     }
 
-    let mut part1 = weights.keys().next().unwrap();
-    let mut part1_parent = parents.get(part1);
-    while part1_parent.is_some() {
-        part1 = part1_parent.unwrap();
-        part1_parent = parents.get(part1);
+    let mut root = weights.keys().next().unwrap();
+    let mut root_parent = parents.get(root);
+    while root_parent.is_some() {
+        root = root_parent.unwrap();
+        root_parent = parents.get(root);
     }
 
-    (part1, part2)
+    let mut summed_weights = FxHashMap::default();
+    get_summed_weights(root, &weights, &children, &mut summed_weights);
+    // eprintln!("{:?}", summed_weights);
+
+    let mut problem = *root;
+    let mut next_problem = is_unbalanced(&children, root, &summed_weights);
+    while next_problem.is_some() {
+        problem = next_problem.unwrap();
+        next_problem = is_unbalanced(&children, problem, &summed_weights);
+    }
+
+    eprintln!("{:?}", problem);
+
+    let problem_parent = *&parents[problem];
+    let problem_parent_children = &children[problem_parent];
+    let nonproblem_child = problem_parent_children
+        .iter()
+        .find(|&&c| c != problem)
+        .unwrap();
+    let target_weight = summed_weights[nonproblem_child];
+    let correct_child_weight =
+        summed_weights[children[problem][0]] * children[problem].len() as u32;
+    dbg!(
+        problem_parent,
+        problem_parent_children,
+        nonproblem_child,
+        target_weight,
+        correct_child_weight
+    );
+    part2 = target_weight - correct_child_weight;
+
+    (root, part2)
+}
+
+fn is_unbalanced<'i>(
+    children: &FxHashMap<&'i str, Vec<&'i str>>,
+    root: &'i str,
+    summed_weights: &FxHashMap<&'i str, u32>,
+) -> Option<&'i str> {
+    let mut bad_proc = root;
+    let mut bad_chld = &children[bad_proc];
+    let weight_counts = bad_chld.iter().map(|c| summed_weights[c]).counts();
+    if weight_counts.len() != 1 {
+        eprintln!("{}'s children are unbalanced", bad_proc);
+        let odd_weight = weight_counts
+            .iter()
+            .find(|(_, &c)| c == 1)
+            .map(|(x, _)| x)
+            .unwrap();
+
+        let odd_proc = bad_chld
+            .iter()
+            .map(|c| (c, summed_weights[c]))
+            .find(|x| x.1 == *odd_weight)
+            .unwrap();
+        return Some(odd_proc.0);
+    } else {
+        eprintln!("{}'s children are balanced", bad_proc);
+        return None;
+    }
+}
+
+fn get_summed_weights<'i>(
+    start: &'i str,
+    weights: &FxHashMap<&'i str, u32>,
+    children: &FxHashMap<&'i str, Vec<&'i str>>,
+    result: &mut FxHashMap<&'i str, u32>,
+) {
+    // eprintln!("{:?}", start);
+    let this_weight = *&weights[start];
+    match children.get(start) {
+        Some(chld) => {
+            chld.iter()
+                .for_each(|c| get_summed_weights(c, weights, children, result));
+            result.insert(
+                start,
+                this_weight + chld.iter().map(|c| result[c]).sum::<u32>(),
+            );
+        }
+        None => {
+            result.insert(start, this_weight);
+        }
+    }
 }
 
 #[test]
@@ -61,5 +149,5 @@ cntj (57)
     let (part1, part2) = solve_for(input);
 
     assert_eq!(part1, "tknk");
-    assert_eq!(part2, 0);
+    assert_eq!(part2, 60);
 }
