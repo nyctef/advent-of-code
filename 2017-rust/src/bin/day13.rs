d40da5ca77b5241a5cee545888df552ec25c8ca4
Nyctef <nyctef@nyctef.com>
2025-01-04 10:50:22 +0000

    day 13 part 1

---
+++ 2017-rust/src/bin/day13.rs
@@ -0,0 +1,56 @@
+use aoc_2017_rust::util::*;
+use color_eyre::eyre::Result;
+use itertools::Itertools;
+
+pub fn main() -> Result<()> {
+    color_eyre::install()?;
+
+    let input = get_input(2017, 13)?;
+
+    let (part1, part2) = solve_for(&input);
+
+    println!("Part 1: {} | Part 2: {}", part1, part2);
+    Ok(())
+}
+
+fn solve_for(input: &str) -> (u32, u64) {
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    let mut scanners = input
+        .trim()
+        .lines()
+        .map(|l| {
+            let nums = all_numbers(l);
+            // depth, length
+            (nums[0], nums[1])
+        })
+        .collect_vec();
+
+   for &(depth, length) in &scanners {
+
+       let cycle = (length - 1) * 2;
+       if depth % cycle == 0 {
+           eprintln!("caught at {depth} with cycle {length}");
+           part1 += length * depth;
+
+       }
+
+   }
+
+    (part1, part2)
+}
+
+#[test]
+fn test_example1() {
+    let input = r###"
+0: 3
+1: 2
+4: 4
+6: 4
+"###;
+    let (part1, part2) = solve_for(input);
+
+    assert_eq!(part1, 24);
+    assert_eq!(part2, 0);
+}
