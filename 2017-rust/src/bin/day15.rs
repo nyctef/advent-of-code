89d474106b2a09d0569b8b2bbbeb9e4ec36f6c0b
Nyctef <nyctef@nyctef.com>
2025-01-07 20:56:31 +0000

    day 15 part 2

--- 2017-rust/src/bin/day15.rs
+++ 2017-rust/src/bin/day15.rs
@@ -1,6 +1,7 @@
 use aoc_2017_rust::util::*;
 use color_eyre::eyre::Result;
-use itertools::Itertools;
+use derive_more::Constructor;
+use itertools::{izip, Itertools};
 
 pub fn main() -> Result<()> {
     color_eyre::install()?;
@@ -15,31 +16,58 @@ pub fn main() -> Result<()> {
 
 fn solve_for(input: &str) -> (u64, u64) {
     let mut part1 = 0;
-    let mut part2 = 0;
 
-    let mut nums = input
+    let nums = input
         .trim()
         .lines()
         .map(|l| all_numbers_u64(l)[0])
         .collect_vec();
 
-    let mut gen1 = nums[0];
-    let mut gen2 = nums[1];
+    let gen1 = Generator::new(16807, 1, nums[0]);
+    let gen2 = Generator::new(48271, 1, nums[1]);
 
     let mask = u16::MAX as u64;
 
-    for _ in 0..40_000_000 {
-        gen1 *= 16807;
-        gen1 %= 2147483647;
-        gen2 *= 48271;
-        gen2 %= 2147483647;
-
-        if gen1 & mask == gen2 & mask {
+    for (_, val1, val2) in izip!(0..40_000_000, gen1, gen2) {
+        if val1 & mask == val2 & mask {
             part1 += 1;
         }
     }
 
+    let mut part2 = 0;
+    let gen1 = Generator::new(16807, 4, nums[0]);
+    let gen2 = Generator::new(48271, 8, nums[1]);
+
+    for (_, val1, val2) in izip!(0..5_000_000, gen1, gen2) {
+        if val1 & mask == val2 & mask {
+            part2 += 1;
+        }
+    }
+
     (part1, part2)
+}
+
+#[derive(Debug, Constructor)]
+struct Generator {
+    factor: u64,
+    filter: u64,
+    val: u64,
+}
+
+impl Iterator for Generator {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            self.val *= self.factor;
+            self.val %= 2147483647;
+
+            if self.val % self.filter == 0 {
+                break;
+            }
+        }
+        Some(self.val)
+    }
 }
 
 #[test]
@@ -51,5 +79,5 @@ Generator B starts with 8921
     let (part1, part2) = solve_for(input);
 
     assert_eq!(part1, 588);
-    assert_eq!(part2, 0);
+    assert_eq!(part2, 309);
 }
