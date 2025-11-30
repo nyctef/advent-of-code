efae555512a3aab4d4c12c3dc98186c3b9cefd3c
Nyctef <nyctef@nyctef.com>
2025-11-30 17:16:13 +0000

    run current day by default

--- 2025-haskell/app/Main.hs
+++ 2025-haskell/app/Main.hs
@@ -2,11 +2,37 @@ module Main where
 
 import qualified Day01
 
+import System.Environment (getArgs)
+import Data.Time.Calendar (toGregorian)
+import Data.Time.Clock (getCurrentTime, utctDay)
+import Text.Read (readMaybe)
+
 main :: IO ()
 main = do
+    args <- getArgs
+    day <- case args of
+        [] -> do
+            -- No argument provided, use current day of month
+            now <- getCurrentTime
+            let (_, _, dayOfMonth) = toGregorian (utctDay now)
+            return dayOfMonth
+        (dayStr:_) -> case readMaybe dayStr of
+            Just d | d >= 1 && d <= 12 -> return d
+            _ -> do
+                putStrLn "Error: Day must be a number between 1 and 12"
+                putStrLn "Usage: aoc2025 [DAY]"
+                return 1  -- Default to day 1 on error
+
     putStrLn "Advent of Code 2025"
     putStrLn "==================="
     putStrLn ""
 
+    runDay day
+
+runDay :: Int -> IO ()
+runDay 1 = do
     putStrLn "Day 1:"
     Day01.solve
+runDay day = do
+    putStrLn $ "Day " ++ show day ++ ":"
+    putStrLn "  Not yet implemented"
