d006a820f1c7748cf1dea691897f047ca304aea0
Nyctef <nyctef@nyctef.com>
2025-12-01 11:20:09 +0000

    indent multiline string
    this indentation should get removed(?)

--- 2025-haskell/test/Day01Test.hs
+++ 2025-haskell/test/Day01Test.hs
@@ -10,16 +10,16 @@ import qualified Day01
 test_part1 :: TestTree
 test_part1 = testCase "Day 1 - Part 1" $ do
   Day01.part1 """
-L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82
+  L68
+  L30
+  R48
+  L5
+  R60
+  L55
+  L1
+  L99
+  R14
+  L82
   """ @?= 3
 
 test_part2 :: TestTree
