a2fbcbd005c89bd328089c055d6f22939fe10cad
Nyctef <nyctef@nyctef.com>
2025-12-01 10:13:38 +0000

    starting day 1: trying to run a test

--- 2025-haskell/test/Day01Test.hs
+++ 2025-haskell/test/Day01Test.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE MultilineStrings #-}
+
 module Day01Test (tests) where
 
 import Test.Tasty
@@ -8,7 +10,18 @@ import qualified Day01
 tests :: TestTree
 tests = testGroup "Day 1"
   [ testCase "Part 1 - placeholder" $ do
-      Day01.part1 "" @?= 0
+      Day01.part1 """
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
+      """ @?= 3
 
   , testCase "Part 2 - placeholder" $ do
       Day01.part2 "" @?= 0
