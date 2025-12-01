66532fbdc1a37175f34b69934a0a3797dc6b6d20
Nyctef <nyctef@nyctef.com>
2025-12-01 09:49:27 +0000

    update haskell package to allow multiline strings extension

--- 2025-haskell/flake.nix
+++ 2025-haskell/flake.nix
@@ -11,7 +11,7 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        haskellPackages = pkgs.haskellPackages;
+        haskellPackages = pkgs.haskell.packages.ghc9122;
 
         packageName = "aoc2025";
       in
