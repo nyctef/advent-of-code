dc5f9cd50ecaf9930d25b8eb54d83aa864bca89f
Nyctef <nyctef@nyctef.com>
2025-12-01 11:23:17 +0000

    show that input is getting passed from the test

--- 2025-haskell/src/Day01.hs
+++ 2025-haskell/src/Day01.hs
@@ -1,9 +1,10 @@
 module Day01 (solve, part1, part2) where
 
 import InputFetcher (getInput)
+import Debug.Trace (trace)
 
 part1 :: String -> Int
-part1 input = 0  -- TODO: implement
+part1 input = trace ("input :" ++ input) 0
 
 part2 :: String -> Int
 part2 input = 0  -- TODO: implement
