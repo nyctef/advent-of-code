da9433886ff8687a637e411e27d6a675b45288c4
Nyctef <nyctef@nyctef.com>
2025-12-07 10:33:38 +0000

    extract GridRC into its own module

--- 2025-haskell/src/Day07.hs
+++ 2025-haskell/src/Day07.hs
@@ -11,22 +11,10 @@ import qualified Data.Text.IO as TIO
 import InputFetcher (getInput)
 import qualified PointRC as P
 import PointRC(PointRC(..))
-
-
--- instance Hashable PointRC where
--- hashWithSalt s (PointRC r c) = s + (hash r) + (hash c)
-data GridRC = GridRC {grid :: HashMap PointRC Char, numCols :: Int, numRows :: Int} deriving (Show)
+import qualified GridRC as G
+import GridRC(GridRC(..))
 
 type Input = GridRC
-
-gpoints :: GridRC -> [PointRC]
-gpoints g = [PointRC r c | r <- [0 .. numRows g], c <- [0 .. numCols g]]
-
-gget :: GridRC -> PointRC -> Maybe Char
-gget g p = HashMap.lookup p $ grid g
-
-gfind :: GridRC -> Char -> Maybe PointRC
-gfind g c = listToMaybe $ fmap fst $ filter ((== c) . snd) $ HashMap.toList $ grid g
 
 -- like traceShow, except that it doesn't
 _traceShow :: a -> b -> b
@@ -42,16 +30,16 @@ solve1 g (q : qs, s, e, c)
   -- optimization: skip a point if it's already seen
   | Set.member q s = _traceShow (q, "skipping" :: String) (qs, s, e, c)
   -- process '.' : just move down
-  | gget g q == Just '.' || gget g q == Just 'S' =
+  | G.get g q == Just '.' || G.get g q == Just 'S' =
       let n = P.down q
        in _traceShow (q, "down" :: String, n : qs) (n : qs, Set.insert q s, e, c)
   -- process '^' : add beams to sides
-  | gget g q == Just '^' =
+  | G.get g q == Just '^' =
       let n1 = P.left q
           n2 = P.right q
        in _traceShow ("sides" :: String) (n1 : n2 : qs, Set.insert q s, e, c + 1)
   -- process off the edge: save an exit point
-  | isNothing (gget g q) = _traceShow (q, "exit" :: String) (qs, s, Set.insert q e, c)
+  | isNothing (G.get g q) = _traceShow (q, "exit" :: String) (qs, s, Set.insert q e, c)
   -- unhandled case?
   | otherwise = error (show (qs, s, e))
 
@@ -63,7 +51,7 @@ run f x
 part1 :: Input -> Int
 part1 input = result
   where
-    start = fromJust $ gfind input 'S'
+    start = fromJust $ G.find input 'S'
     step :: State1 -> State1
     step = solve1 input
     seed :: State1
@@ -78,11 +66,11 @@ solve2 g s p =
     _traceShow (p, fromleft, fromup, fromright) HashMap.insert p (fromleft + fromup + fromright) s
   where
     upleftP = (P.up . P.left) p
-    upleftC = gget g upleftP
+    upleftC = G.get g upleftP
     upP = P.up p
-    upC = gget g upP
+    upC = G.get g upP
     uprightP = (P.up . P.right) p
-    uprightC = gget g uprightP
+    uprightC = G.get g uprightP
     fromleft = if upleftC == Just '^' then HashMap.findWithDefault 0 upleftP s else 0
     fromup = if upC == Just '.' || upC == Just 'S' then HashMap.findWithDefault 0 upP s else 0
     fromright = if uprightC == Just '^' then HashMap.findWithDefault 0 uprightP s else 0
@@ -90,12 +78,12 @@ solve2 g s p =
 part2 :: Input -> Int
 part2 input = result
   where
-    start = fromJust $ gfind input 'S'
+    start = fromJust $ G.find input 'S'
     seed :: HashMap PointRC Int
     seed = HashMap.fromList [(start, 1)]
-    points = filter (\p -> P.row p /= 0) (gpoints input)
+    points = filter (\p -> P.row p /= 0) (G.points input)
     state = foldl (solve2 input) seed points
-    bottomRow = map (PointRC (numRows input - 1)) [0..numCols input - 1]
+    bottomRow = map (PointRC (G.numRows input - 1)) [0..G.numCols input - 1]
     total = sum $ map (\p -> HashMap.findWithDefault 0 p state) bottomRow
 
     result = _traceShow state total
@@ -104,13 +92,7 @@ tshow :: (Show a) => a -> Text
 tshow = T.pack . show
 
 parseInput :: Text -> Either String Input
-parseInput i =
-  let ls = T.lines i
-      cols = map (zip [0 ..] . T.unpack) ls
-      rows = zip [0 ..] cols
-      cells = [(PointRC r c, val) | (r, cs) <- rows, (c, val) <- cs]
-      hashmap = HashMap.fromList cells
-   in Right $ GridRC hashmap (length $ fromJust $ listToMaybe cols) (length cols)
+parseInput = G.parse 
 
 solve :: IO ()
 solve = do
---
+++ 2025-haskell/src/GridRC.hs
@@ -0,0 +1,29 @@
+module GridRC (GridRC(GridRC, numCols, numRows), points, get, find, parse) where
+
+import Data.HashMap.Strict (HashMap)
+import qualified Data.HashMap.Strict as HashMap
+import Data.Maybe
+import Data.Text (Text)
+import qualified Data.Text as T
+import PointRC(PointRC(..))
+
+data GridRC = GridRC {grid :: HashMap PointRC Char, numCols :: Int, numRows :: Int} deriving (Show)
+
+
+points :: GridRC -> [PointRC]
+points g = [PointRC r c | r <- [0 .. numRows g], c <- [0 .. numCols g]]
+
+get :: GridRC -> PointRC -> Maybe Char
+get g p = HashMap.lookup p $ grid g
+
+find :: GridRC -> Char -> Maybe PointRC
+find g c = listToMaybe $ fmap fst $ filter ((== c) . snd) $ HashMap.toList $ grid g
+
+parse :: Text -> Either String GridRC
+parse i =
+  let ls = T.lines i
+      cols = map (zip [0 ..] . T.unpack) ls
+      rows = zip [0 ..] cols
+      cells = [(PointRC r c, val) | (r, cs) <- rows, (c, val) <- cs]
+      hashmap = HashMap.fromList cells
+   in Right $ GridRC hashmap (length $ fromJust $ listToMaybe cols) (length cols)
